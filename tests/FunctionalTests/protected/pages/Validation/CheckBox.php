101d1733f1ed5421161ee40c02d91e55ef9eafcb
xue <>
2006-01-22 00:17:02 +0000

    Added tests for checkbox with validation.

---
+++ tests/FunctionalTests/protected/pages/Validation/CheckBox.php
@@ -0,0 +1,31 @@
+<?php
+
+class CheckBox extends TPage
+{
+	protected function onLoad($param)
+	{
+		if(!$this->IsPostBack)
+			$this->dataBind();
+	}
+}
+
+class CheckBoxTestCase extends SeleniumTestCase
+{
+	function setup()
+	{
+	    $this->open(Prado::getApplication()->getTestPage(__FILE__));
+	}
+
+	function testValidator()
+	{
+		$this->assertTextPresent("Basic TRequiredFieldValidator Test1");
+		$this->assertNotVisible("validator1");
+		$this->click("button1");
+		$this->assertVisible("validator1");
+		$this->type("text1", "test");
+		$this->clickAndWait("button1");
+		$this->assertNotVisible("validator1");
+	}
+}
+
+?>
