230067c5425f9e95e601d682a8b9bb523190856e
xue <>
2006-01-24 04:04:46 +0000

    trying to use verifyHTMLPresent

--- tests/FunctionalTests/quickstart_tests/Controls/HyperLinkTestCase.php
+++ tests/FunctionalTests/quickstart_tests/Controls/HyperLinkTestCase.php
@@ -6,7 +6,8 @@ class HyperLinkTestCase extends SeleniumTestCase
 	{
 		$this->open("../../demos/quickstart/index.php?page=Controls.Samples.THyperLink.Home&functionaltest=true", "");
 		$this->verifyTitle("PRADO QuickStart Sample", "");
-		$this->verifyAttribute("//a[@href=\"http://www.pradosoft.com/\"]/@target","_blank");
+		//$this->verifyAttribute("//a[@href=\"http://www.pradosoft.com/\"]/@target","_blank");
+		$this->verifyHTMLPresent('<a href="http://www.pradosoft.com/" target="_blank" style="background-color:silver;font-style:italic;">PradoSoft.com</a>');
 		$this->verifyTextPresent("Welcome to", "");
 		$this->verifyTextPresent("Body contents", "");
 		$this->verifyElementPresent("//a[img/@alt='Hello World']");
