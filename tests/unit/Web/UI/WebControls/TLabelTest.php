4724cb4ed35c6cdb31e2c3381002f62bd6530e5a
knut <>
2006-02-02 22:49:40 +0000

    removed checking getTagName() since it's protected

--- tests/unit/Web/UI/WebControls/TLabelTest.php
+++ tests/unit/Web/UI/WebControls/TLabelTest.php
@@ -8,14 +8,6 @@ Prado::using('System.Web.UI.THtmlWriter');
  * @package System.Web.UI.WebControls
  */
 class TLabelTest extends PHPUnit2_Framework_TestCase {
-  
-  public function testGetTagName() {
-    $label = new TLabel();
-    $this->assertEquals('span', $label->getTagName());
-
-    $label->setForControl('Test');
-    $this->assertEquals('label', $label->getTagName());
-  }
 
   public function testSetText() {
     $label = new TLabel();
