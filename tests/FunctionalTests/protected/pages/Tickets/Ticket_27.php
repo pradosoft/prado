4422f22094e6cfc8c34e391ab0ec7a3328cbf4cf
wei <>
2006-02-02 07:14:06 +0000

    FTest for #27

--- tests/FunctionalTests/protected/pages/Tickets/Ticket_27.php
+++ tests/FunctionalTests/protected/pages/Tickets/Ticket_27.php
@@ -12,6 +12,13 @@ class Ticket_27_TestCase extends SeleniumTestCase
 	function test()
 	{
 		$this->open($this->getPage($this));
+		$this->verifyTitle("Verifying Ticket 27", "");
+		$this->click("//input[@value='Agree']", "");
+		$this->assertVisible("ctl0_Content_validator1", "");
+		$this->type("ctl0_Content_TextBox", "122");
+		$this->assertNotVisible("ctl0_Content_validator1", "");
+		$this->clickAndWait("//input[@value='Disagree']", "");
+		$this->assertNotVisible("ctl0_Content_validator1", "");
 	}
 }
 
