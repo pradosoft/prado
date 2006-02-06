57f7e6924dc8e996a06b46418e8f98f4d0a9531b
knut <>
2006-02-06 20:01:14 +0000

    added PHPUnit2_Framework_IncompleteTestError

--- tests/unit/phpunit2.php
+++ tests/unit/phpunit2.php
@@ -6,4 +6,5 @@ define('PRADO_FRAMEWORK_DIR', dirname(__FILE__).'/../../framework');
 set_include_path(PRADO_FRAMEWORK_DIR.':'.get_include_path());
 require_once PRADO_FRAMEWORK_DIR.'/prado.php';
 require_once 'PHPUnit2/Framework/TestCase.php';
+require_once 'PHPUnit2/Framework/IncompleteTestError.php';
 ?>
