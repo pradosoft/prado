5773b275b4a79bb31471d43faf7f60f380897968
knut <>
2006-04-24 21:34:00 +0000

    Added a few comments on unit tests

--- tests/unit/phpunit2.php
+++ tests/unit/phpunit2.php
@@ -1,6 +1,14 @@
 <?php
 /**
  * A few common settings for all unit tests.
+ *
+ * This file should be included in all unit tests with absolute path to be able to run it
+ * from the command line with "phpunit" like this:
+ *
+ * require_once dirname(__FILE__).'/path/../to/../phpunit2.php';
+ *
+ * Also remember do define the @package attribute for your test class to make it appear under
+ * the right package in unit test and code coverage reports.
  */
 define('PRADO_FRAMEWORK_DIR', dirname(__FILE__).'/../../framework');
 set_include_path(PRADO_FRAMEWORK_DIR.':'.get_include_path());
