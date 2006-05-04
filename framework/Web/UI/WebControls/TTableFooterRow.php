ad005a0cfb526a41d04dd5260df654845ef68f5b
xue <>
2006-05-04 13:45:09 +0000

    Added table section support.

---
+++ framework/Web/UI/WebControls/TTableFooterRow.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * TTableFooterRow class file
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @link http://www.pradosoft.com/
+ * @copyright Copyright &copy; 2005 PradoSoft
+ * @license http://www.pradosoft.com/license/
+ * @version $Revision: $  $Date: $
+ * @package System.Web.UI.WebControls
+ */
+
+/**
+ * Includes TTableRow class.
+ */
+Prado::using('System.Web.UI.WebControls.TTableRow');
+
+/**
+ * TTableFooterRow class.
+ *
+ * TTableFooterRow displays a table footer row.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @version $Revision: $  $Date: $
+ * @package System.Web.UI.WebControls
+ * @since 3.0.1
+ */
+class TTableFooterRow extends TTableRow
+{
+	/**
+	 * @return string location of a row in a table. Always returns 'Footer'.
+	 */
+	public function getTableSection()
+	{
+		return 'Footer';
+	}
+
+	/**
+	 * @param string location of a row in a table.
+	 * @throws TInvalidOperationException if this method is invoked
+	 */
+	public function setTableSection($value)
+	{
+		throw new TInvalidOperationException('tablefooterrow_tablesection_readonly');
+	}
+}
+
+?>
