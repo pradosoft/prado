db7d5e7d65bba035a5adedc84d683f3195bf27ea
wei <>
2006-05-05 08:23:20 +0000

    Adding TActivePanel

---
+++ framework/Web/UI/ActiveControls/TActivePanel.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Created on 5/05/2006
+ */
+
+class TActivePanel extends TPanel
+{
+	/**
+	 * Creates a new callback control, sets the adapter to
+	 * TActiveControlAdapter. If you override this class, be sure to set the
+	 * adapter appropriately by, for example, by calling this constructor.
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->setAdapter(new TActiveControlAdapter($this));
+	}
+} 
+
+?>
