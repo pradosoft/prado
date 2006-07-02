4009bc98e96c38d22b3c199f3bfefe4c30783d00
wei <>
2006-07-02 04:51:24 +0000

    Add Ticket tests

--- tests/FunctionalTests/index.php
+++ tests/FunctionalTests/index.php
@@ -8,8 +8,8 @@ Prado Functional Test Suites
 <h1>Prado Functional Test Suites</h1>
 <ul>
   <li><a href="quickstart.php">Tests of QuickStart Tutorial Demo</a></li>
-  <li><a href="validators.php">Tests of Validators</a></li>
-  <li><a href="tickets.php">Tests of Trac Tickets</a></li>
+  <li><a href="validators.php">Tests of Validators</a> (<a href="validators/index.php">test samples</a>)</li>
+  <li><a href="tickets.php">Tests of Trac Tickets</a> (<a href="tickets/index.php">test samples</a>)</li>
   <li><a href="features.php">Tests of New Features</a> (<a href="features/index.php">list of new features</a>)</li>
 </ul>
 </body>
