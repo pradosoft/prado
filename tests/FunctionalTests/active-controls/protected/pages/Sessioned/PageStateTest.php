91fe694c8755aa2d05743946a3681be7232420b6
wei <>
2006-12-04 00:17:43 +0000

    propage changes to OnError and OnSuccess for validators ClientSide.

--- tests/FunctionalTests/active-controls/protected/pages/Sessioned/PageStateTest.php
+++ tests/FunctionalTests/active-controls/protected/pages/Sessioned/PageStateTest.php
@@ -4,8 +4,9 @@ class PageStateTest extends TPage
 {
 	function button1_oncallback($sender, $param)
 	{
-		sleep(rand(0,5));
-		$this->label1->Text .= " button1 clicked ";
+		//sleep(rand(0,5));
+		$sender->CustomData = $sender->CustomData + 1;
+		$this->label1->Text = " button1 clicked ".$sender->CustomData." times";
 	}
 }
 
