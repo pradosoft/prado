176ee7e9b6c69cf42e7afeeb03db602237a49bc3
xue <>
2006-12-05 20:38:18 +0000

    Added TApplicationStateCacheDependency.

--- framework/Caching/TCache.php
+++ framework/Caching/TCache.php
@@ -599,6 +599,36 @@ class TChainedCacheDependency extends TCacheDependency
 
 
 /**
+ * TApplicationStateCacheDependency class.
+ *
+ * TApplicationStateCacheDependency performs dependency checking based on
+ * the mode of the currently running PRADO application.
+ * The dependency is reportedly as unchanged if and only if the application
+ * is running in performance mode.
+ *
+ * You may chain this dependency together with other dependencies
+ * so that only when the application is not in performance mode the other dependencies
+ * will be checked.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @version $Id$
+ * @package System.Caching
+ * @since 3.1.0
+ */
+class TApplicationStateCacheDependency extends TCacheDependency
+{
+	/**
+	 * Performs the actual dependency checking.
+	 * This method returns true if the currently running application is not in performance mode.
+	 * @return boolean whether the dependency is changed or not.
+	 */
+	public function getHasChanged()
+	{
+		return Prado::getApplication()->getMode()!==TApplicationMode::Performance;
+	}
+}
+
+/**
  * TCacheDependencyList class.
  *
  * TCacheDependencyList represents a list of cache dependency objects.
