8dc16587c5ef737d6a2aa3cdb142e49bc7b8c8ec
xue <>
2007-01-01 17:14:36 +0000

    added requirement check for TSoapService.

--- requirements/index.php
+++ requirements/index.php
@@ -114,7 +114,16 @@ $requirements = array(
 		extension_loaded("mcrypt"),
 		'Mcrypt extension check',
 		'Mcrypt extension optional'),
-	array(false,'extension_loaded("xsl")','XSL extension check','XSL extension optional'),
+	array(
+		false,
+		extension_loaded("xsl"),
+		'XSL extension check',
+		'XSL extension optional'),
+	array(
+		false,
+		extension_loaded("soap"),
+		'SOAP extension check',
+		'SOAP extension optiona'),
 );
 
 $results = "<table class=\"result\">\n";
