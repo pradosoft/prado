74acb5d2a4426cb4ae7f0fdd86e23c1459813a60
xue <>
2007-01-17 16:45:16 +0000

    updated build script and prado powered link target.

--- framework/PradoBase.php
+++ framework/PradoBase.php
@@ -105,7 +105,7 @@ class PradoBase
 	 */
 	public static function poweredByPrado()
 	{
-		return '<a title="Powered by PRADO" href="http://www.pradosoft.com/"><img src="http://www.pradosoft.com/images/powered.gif" style="border-width:0px;" alt="Powered by PRADO" /></a>';
+		return '<a title="Powered by PRADO" href="http://www.pradosoft.com/"><img src="http://www.pradosoft.com/images/powered.gif" style="border-width:0px;" alt="Powered by PRADO" target="_blank" /></a>';
 	}
 
 	/**
