8d156610be187427d43f4d033b258f060f8968ad
xue <>
2007-02-09 19:02:43 +0000

    reorganize personal demo.

--- demos/personal/protected/Pages/Layout.php
+++ demos/personal/protected/Pages/Layout.php
@@ -2,11 +2,6 @@
 
 class Layout extends TTemplateControl
 {
-	public function logout($sender,$param)
-	{
-		$this->Application->getModule('auth')->logout();
-		$this->Response->redirect($this->Service->constructUrl('Home',null,false));
-	}
 }
 
 ?>
