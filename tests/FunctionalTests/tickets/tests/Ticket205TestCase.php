b0ee562087936a5b139457494f18bbf8362e257e
xue <>
2007-03-26 00:29:27 +0000

    merge from 3.0 branch till 1771.

--- tests/FunctionalTests/tickets/tests/Ticket205TestCase.php
+++ tests/FunctionalTests/tickets/tests/Ticket205TestCase.php
@@ -8,14 +8,14 @@ class Ticket205TestCase extends SeleniumTestCase
 		$this->open("tickets/index.php?page=Ticket205");
 		$this->assertTitle("Verifying Ticket 205");
 		$this->assertNotVisible("{$base}validator1");
-		
+
 		$this->type("{$base}textbox1", "test");
 		$this->click("{$base}button1");
-		$this->assertVisible("{$base}validator1");
 		$this->assertAlert("error");
-		
+		$this->assertVisible("{$base}validator1");
+
 		$this->type("{$base}textbox1", "Prado");
-		$this->clickAndWait("{$base}button1");		
+		$this->clickAndWait("{$base}button1");
 		$this->assertNotVisible("{$base}validator1");
 	}
 }
