8afa142d90de10cbd8bbd840c8eb32a3f5b846af
knut <>
2007-05-29 20:54:31 +0000

    completed THttpUtilityTest

--- tests/unit/Web/THttpUtilityTest.php
+++ tests/unit/Web/THttpUtilityTest.php
@@ -8,12 +8,16 @@ Prado::using('System.Web.THttpUtility');
  */
 class THttpUtilityTest extends PHPUnit_Framework_TestCase {
 
-  public function testHtmlEncode() {
-    throw new PHPUnit_Framework_IncompleteTestError();
-  }
+	public function testHtmlEncode() {
+		$html = THttpUtility::htmlEncode('<tag key="value">');
+		self::assertEquals('&lt;tag key=&quot;value&quot;&gt;', $html);
+		$html = THttpUtility::htmlEncode('&lt;');
+		self::assertEquals('&lt;', $html);
+	}
 
-  public function testHtmlDecode() {
-    throw new PHPUnit_Framework_IncompleteTestError();
-  }
+	public function testHtmlDecode() {
+    	$html = THttpUtility::htmlDecode('&lt;tag key=&quot;value&quot;&gt;');
+		self::assertEquals('<tag key="value">', $html);
+	}
 }
 ?>
