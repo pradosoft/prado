8c30dccc451beeeb74993bdd5ecdf405acdc6062
knut <>
2007-06-12 08:58:24 +0000

    added stubs for TClientScriptManagerTest

---
+++ tests/unit/Web/UI/TClientScriptManagerTest.php
@@ -0,0 +1,148 @@
+<?php
+require_once dirname(__FILE__).'/../../phpunit.php';
+
+Prado::using('System.Web.UI.TClientScriptManager');
+
+/**
+ * @package System.Web.UI
+ */
+class TClientScriptManagerTest extends PHPUnit_Framework_TestCase {
+
+  public function testConstruct() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterPradoScript() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testGetPradoScriptAssetUrl() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterJavascriptPackages() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testGetCallbackReference() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterCallbackControl() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterPostBackControl() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterDefaultButton() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterFocusControl() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterStyleSheetFile() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterStyleSheet() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterHeadScriptFile() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterHeadScript() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterScriptFile() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterBeginScript() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterEndScript() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRegisterHiddenField() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testIsStyleSheetFileRegistered() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testIsStyleSheetRegistered() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testIsHeadScriptFileRegistered() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testIsHeadScriptRegistered() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testIsScriptFileRegistered() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testIsBeginScriptRegistered() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testIsEndScriptRegistered() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testHasEndScripts() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testHasBeginScripts() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testIsHiddenFieldRegistered() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRenderStyleSheetFiles() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRenderStyleSheets() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRenderHeadScriptFiles() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRenderHeadScripts() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRenderScriptFiles() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRenderBeginScripts() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+  public function testRenderEndScripts() {
+    throw new PHPUnit_Framework_IncompleteTestError();
+  }
+
+}
+?>
