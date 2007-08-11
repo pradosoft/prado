8011f9cec174c781bd975bb4c7094da2e0209bae
xue <>
2007-08-11 19:22:41 +0000

    fixed path_info bug.

--- framework/Web/THttpRequest.php
+++ framework/Web/THttpRequest.php
@@ -187,7 +187,7 @@ class THttpRequest extends TApplicationComponent implements IteratorAggregate,Ar
 
 		if(isset($_SERVER['PATH_INFO']))
 			$this->_pathInfo=$_SERVER['PATH_INFO'];
-		else if(strpos($_SERVER['PHP_SELF'],$_SERVER['SCRIPT_NAME'])===0)
+		else if(strpos($_SERVER['PHP_SELF'],$_SERVER['SCRIPT_NAME'])===0 && $_SERVER['PHP_SELF']!==$_SERVER['SCRIPT_NAME'])
 			$this->_pathInfo=substr($_SERVER['PHP_SELF'],strlen($_SERVER['SCRIPT_NAME']));
 		else
 			$this->_pathInfo='';
