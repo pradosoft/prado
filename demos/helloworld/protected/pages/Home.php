91536b68a8eee2e2849949f605d3e5428f71cd9c
xue <>
2007-08-29 03:15:30 +0000

    revert.

--- demos/helloworld/protected/pages/Home.php
+++ demos/helloworld/protected/pages/Home.php
@@ -2,18 +2,9 @@
 
 class Home extends TPage
 {
-	public function onPreRender($param)
-	{
-		echo "token is |".$this->Captcha->Token."|";
-	}
-
 	public function buttonClicked($sender,$param)
 	{
-		if($this->Captcha->validate($this->Input->Text))
-			$sender->Text="ok";
-		else
-			$sender->Text="no!";
-		$this->Captcha->regenerateToken();
+		$sender->Text='Hello world!';
 	}
 }
 
