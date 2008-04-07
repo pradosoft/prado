b43c8b4c9f3973eb5482298ed8283e33c1f426e1
mikl <>
2008-04-07 21:56:09 +0000

    Added test case for #595

---
+++ tests/FunctionalTests/tickets/tests/Ticket595TestCase.php
@@ -0,0 +1,39 @@
+<?php
+
+class Ticket595TestCase extends SeleniumTestCase
+{
+	function test()
+	{
+		$base="ctl0_Content_";
+		$this->open('tickets/index.php?page=Ticket595');
+		$this->assertTitle("Verifying Ticket 595");
+		
+		$this->click($base.'ctl2');
+        $this->assertAttribute($base.'A@class','errorclassA');
+		
+		$this->type($base.'A', 'Prado');
+		$this->click($base.'ctl2');
+        $this->assertAttribute($base.'A@class','errorclassA');
+		
+		$this->type($base.'A', 'test@example.com');
+		$this->click($base.'ctl2');
+		$this->pause(800);
+        $this->assertAttribute($base.'A@class','');
+
+
+		$this->click($base.'ctl5');
+		$this->pause(800);
+        $this->assertAttribute($base.'B@class','errorclassB');
+		
+		$this->type($base.'B', 'Prado');
+		$this->click($base.'ctl5');
+		$this->pause(800);
+        $this->assertAttribute($base.'B@class','errorclassB');
+		
+		$this->type($base.'B', 'test@example.com');
+		$this->click($base.'ctl5');
+		$this->pause(800);
+        $this->assertAttribute($base.'B@class','');
+	}
+}
+?>
