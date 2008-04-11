4ab766dc6ca1d5e294d2b2f34d2198ce9583fbd2
tof <>
2008-04-11 08:23:36 +0000

    TestCase for #828

---
+++ tests/FunctionalTests/tickets/tests/Ticket828TestCase.php
@@ -0,0 +1,41 @@
+<?php
+class Ticket828TestCase extends SeleniumTestCase
+{
+
+	function test()
+	{
+		//problem with test runner clicking on radio buttons
+		$this->skipBrowsers(self::OPERA);
+
+		$base = "ctl0_Content_";
+		$this->open("tickets/index.php?page=Ticket828");
+		$this->click("{$base}submit1");
+		$this->assertVisible("{$base}validator1");
+		$this->assertVisible("{$base}validator2");
+		$this->assertVisible("{$base}validator3");
+		$this->click("{$base}list1_c0");
+		$this->addSelection("{$base}list2", "label=One");
+		$this->addSelection("{$base}list2", "label=Two");
+		$this->click("{$base}list3_c3");
+		$this->clickAndWait("{$base}submit1");
+		$this->assertNotVisible("{$base}validator1");
+		$this->assertNotVisible("{$base}validator2");
+		$this->assertNotVisible("{$base}validator3");
+		$this->click("{$base}list1_c1");
+		$this->click("{$base}list1_c2");
+		$this->click("{$base}list1_c3");
+		$this->addSelection("{$base}list2", "label=Two");
+		$this->click("{$base}list1_c3");
+		$this->clickAndWait("{$base}submit1");
+		$this->assertNotVisible("{$base}validator1");
+		$this->assertNotVisible("{$base}validator2");
+		$this->assertNotVisible("{$base}validator3");
+		$this->click("{$base}list3_c3");
+		$this->clickAndWait("{$base}submit1");
+		$this->pause(200);
+		$this->assertNotVisible("{$base}validator1");
+		$this->assertNotVisible("{$base}validator2");
+		$this->assertNotVisible("{$base}validator3");
+	}
+}
+?>
