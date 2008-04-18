8e4607f6e92258738c5293b3d7609d34bec73969
mikl <>
2008-04-18 16:53:13 +0000

    Updated test case for #660

--- tests/FunctionalTests/tickets/protected/pages/Ticket660.php
+++ tests/FunctionalTests/tickets/protected/pages/Ticket660.php
@@ -12,5 +12,15 @@ class Ticket660 extends TPage {
         $this->A->setText($this->T->getText() . $iso_text);
     }
 
+	public function suggest($sender, $param)
+	{
+        $token=$param->getCallbackParameter();
+		$sender->setDataSource(array(
+            1 => 'ABC Token:'.$token,
+            2 => iconv('UTF-8', 'ISO-8859-1//IGNORE', 'ÄÖÜ').' Token:'.$token
+        ));
+		$sender->dataBind();
+	}
+
 }
 ?>
