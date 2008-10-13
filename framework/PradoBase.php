6967127b27f56b6c12d6df6c48e0800cb3c4156f
carl <>
2008-10-13 13:59:17 +0000

    #913 - PRADO Copyright notice in HTML source

--- framework/PradoBase.php
+++ framework/PradoBase.php
@@ -114,6 +114,11 @@ class PradoBase
 			$url='http://www.pradosoft.com/images/'.$logoName.'.gif';
 		return '<a title="Powered by PRADO" href="http://www.pradosoft.com/" target="_blank"><img src="'.$url.'" style="border-width:0px;" alt="Powered by PRADO" /></a>';
 	}
+	
+	public static function metaGenerator()
+	{
+		return 'PRADO - http://www.pradosoft.com/';
+	}
 
 	/**
 	 * PHP error handler.
