798783263a8638675c0df2d1274fb1947ef79d1b
godzilla80@gmx.net <>
2009-03-18 09:15:53 +0000

    a little "goggle code issues" releated modification

--- tests/FunctionalTests/tickets/protected/pages/Layout.php
+++ tests/FunctionalTests/tickets/protected/pages/Layout.php
@@ -4,10 +4,19 @@ class Layout extends TTemplateControl
 {
 	public function onLoad($param)
 	{
-		$num = str_replace('Ticket','',$this->getPage()->getPagePath());
-		$this->getPage()->setTitle("Verifying Ticket $num");
-		$this->ticketlink->setText("Verifying Ticket $num");
-		$this->ticketlink->setNavigateUrl("http://trac.pradosoft.com/prado/ticket/{$num}");
+		$num	= str_replace(array('Ticket', 'Issue'), '', $this->getPage()->getPagePath());
+		$type	= str_replace($num, '', $this->getPage()->getPagePath());
+		
+		$this->getPage()->setTitle("Verifying $type $num");
+		$this->ticketlink->setText("Verifying $type $num");
+		
+		if(strToLower($type) === 'issue') {
+			$this->ticketlink->setNavigateUrl("http://code.google.com/p/prado3/issues/detail?id={$num}");	
+		}
+		else {
+			$this->ticketlink->setNavigateUrl("http://trac.pradosoft.com/prado/ticket/{$num}");	
+		}
+		
 	}
 }
 
