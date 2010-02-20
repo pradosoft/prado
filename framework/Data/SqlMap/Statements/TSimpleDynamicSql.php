a1d65f3737980658e9a5dd12165860e35e435941
godzilla80@gmx.net <>
2010-02-20 09:18:40 +0000

    Fixed Issue 209 - SqlMap doesn't escape inline params properly

--- framework/Data/SqlMap/Statements/TSimpleDynamicSql.php
+++ framework/Data/SqlMap/Statements/TSimpleDynamicSql.php
@@ -32,9 +32,8 @@ class TSimpleDynamicSql extends TStaticSql
 		foreach($this->_mappings as $property)
 		{
 			$value = TPropertyAccess::get($parameter, $property);
-			$sql = preg_replace('/'.TSimpleDynamicParser::DYNAMIC_TOKEN.'/', $value, $sql, 1);
+			$sql = preg_replace('/'.TSimpleDynamicParser::DYNAMIC_TOKEN.'/', str_replace('$', '\$', $value), $sql, 1);
 		}
-
 		return $sql;
 	}
 }
