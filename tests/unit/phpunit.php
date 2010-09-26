546221db4bcf502c3b5744a438d09cbf9df43070
javalizard <>
2010-09-26 03:50:58 +0000

    Upgraded the phpunit ini file to work with PHPUnit 3.3, 3.4, and 3.5.  There are conditionals for 3.3 and 3.4.  No more Framework file includes are needed with 3.5.

--- tests/unit/phpunit.php
+++ tests/unit/phpunit.php
@@ -15,9 +15,21 @@ set_include_path(PRADO_FRAMEWORK_DIR.PATH_SEPARATOR.get_include_path());
 
 require_once dirname(__FILE__).'/Prado.php';
 require_once PRADO_FRAMEWORK_DIR.'/prado.php';
-require_once 'PHPUnit/Framework/TestCase.php';
-require_once 'PHPUnit/Framework/IncompleteTestError.php';
-require_once 'PHPUnit/Framework/TestSuite.php';
+
+//grab the version of phpunit, OPP: phpunit provided version call
+$version = `{$_SERVER['argv'][0]} --version`;
+preg_match('/(\d\d?)\.(\d\d?)(\.(\d\d?))?/', $version, $matches);
+
+if($matches[1] <= 3){
+	if($matches[2] <= 4){
+		require_once 'PHPUnit/Framework.php';
+		if($matches[2] <= 3){
+			require_once 'PHPUnit/Framework/TestCase.php';
+			require_once 'PHPUnit/Framework/IncompleteTestError.php';
+			require_once 'PHPUnit/Framework/TestSuite.php';
+		}
+	}
+}
 require_once 'PHPUnit/TextUI/TestRunner.php';
 require_once 'PHPUnit/Util/Filter.php';
 ?>
