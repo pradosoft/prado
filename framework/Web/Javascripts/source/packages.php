3ef4e5b9dd7500fbd8125f8a6e382891bead9aca
ctrlaltca@gmail.com <>
2011-05-10 20:39:33 +0000

    updated prototype to 1.7, scriptaculous to 1.9.0 and dropped json.js for json2.js. fixes #199 and 272

--- framework/Web/Javascripts/source/packages.php
+++ framework/Web/Javascripts/source/packages.php
@@ -3,8 +3,8 @@
 //$Id$
 
 // To make future upgrades easier
-if (!defined('PROTOTYPE_DIR')) define ('PROTOTYPE_DIR', 'prototype-1.6.0.3');
-if (!defined('SCRIPTACULOUS_DIR')) define ('SCRIPTACULOUS_DIR', 'scriptaculous-1.8.2');
+if (!defined('PROTOTYPE_DIR')) define ('PROTOTYPE_DIR', 'prototype-1.7');
+if (!defined('SCRIPTACULOUS_DIR')) define ('SCRIPTACULOUS_DIR', 'scriptaculous-1.9.0');
 
 //package names and its contents (files relative to the current directory)
 $packages = array(
@@ -39,7 +39,7 @@ $packages = array(
 
 	'ajax' => array(
 		SCRIPTACULOUS_DIR.'/controls.js',
-		'prado/activecontrols/json.js',
+		'prado/activecontrols/json2.js',
 		'prado/activecontrols/ajax3.js',
 		'prado/activecontrols/activecontrols3.js',
 		'prado/activecontrols/inlineeditor.js'
