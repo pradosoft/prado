1cd0bbc8cda0491baa46cf2a79c9e67e00c5245d
ctrlaltca <>
2012-08-30 14:18:57 +0000

    Prado.Element.setAttribute() : remove the "readonly" attribute when it's set to false (the same already applies for "disabled")

--- framework/Web/Javascripts/source/prado/scriptaculous-adapter.js
+++ framework/Web/Javascripts/source/prado/scriptaculous-adapter.js
@@ -283,7 +283,7 @@ Prado.Element =
 	{
 		var el = $(element);
 		if(!el) return;
-		if((attribute == "disabled" || attribute == "multiple") && value==false)
+		if((attribute == "disabled" || attribute == "multiple" || attribute == "readonly") && value==false)
 			el.removeAttribute(attribute);
 		else if(attribute.match(/^on/i)) //event methods
 		{
