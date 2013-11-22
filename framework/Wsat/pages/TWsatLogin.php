ab6effd64caefa732ef0994d6c3048f5cd474bf6
Daniel <darthdaniel85@gmail.com>
2013-11-22 15:25:30 -0500

    WSAT beta release.

--- framework/Wsat/pages/TWsatLogin.php
+++ framework/Wsat/pages/TWsatLogin.php
@@ -8,11 +8,8 @@
 class TWsatLogin extends TPage {
 
     public function login() {
-        $config_pass = $this->getService()->getPassword();
-        $user_pass = $this->password->Text;
-
-        if ($user_pass === $config_pass) {
-            $this->Session["wsat_password"] = $config_pass;
+        if ($this->IsValid) {
+            $this->Session["wsat_password"] = $this->getService()->getPassword();
 
             $authManager = $this->Application->getModule('auth');
             $url = $authManager->ReturnUrl;
@@ -20,9 +17,13 @@ class TWsatLogin extends TPage {
                 $url = $this->Service->constructUrl('TWsatHome');
             }
             $this->Response->redirect($url);
-        } else {
-            echo "user or pass wrong";
         }
+    }
+
+    public function validatePassword($sender, $param) {
+        $config_pass = $this->getService()->getPassword();
+        $user_pass = $this->password->Text;
+        $param->IsValid = $user_pass === $config_pass;
     }
 
 }
