781f6731122cf622bcdf64441d247b5e37a2c3d0
Fabio Bas <ctrlaltca@gmail.com>
2014-01-12 01:37:33 +0100

    Added some TCallbackClientScript demos

--- demos/quickstart/protected/pages/ActiveControls/Samples/TCallbackClientScript/Home.php
+++ demos/quickstart/protected/pages/ActiveControls/Samples/TCallbackClientScript/Home.php
@@ -8,7 +8,7 @@ class Home extends TPage
 		switch($this->radio1->SelectedValue)
 		{
 			case 1:
-				$this->getCallbackClient()->evaluateScript("<script> alert('something'); </script>");
+				$this->getCallbackClient()->evaluateScript("alert('something');");
 				break;
 			case 2:
 				$this->getCallbackClient()->check($this->check1, !$this->check1->Checked);
@@ -20,8 +20,74 @@ class Home extends TPage
 				$this->getCallbackClient()->show($this->label1);
 				break;
 			case 5:
+				$this->getCallbackClient()->toggle($this->label1);
+				break;
+			case 6:
+				$this->getCallbackClient()->toggle($this->label1, 'fade');
+				break;
+			case 7:
+				$this->getCallbackClient()->toggle($this->label1, 'slide');
+				break;
+			case 8:
+				$this->getCallbackClient()->highlight($this->label1);
+				break;
+			case 9:
 				$this->getCallbackClient()->focus($this->txt1);
 				break;
+			case 10:
+				$this->getCallbackClient()->scrollTo($this->check1, array('duration' => 1000, 'offset' => 10));
+				break;
+			case 11:
+				$this->getCallbackClient()->addCssClass($this->txt1, 'red_background');
+				break;
+			case 12:
+				$this->getCallbackClient()->removeCssClass($this->txt1, 'red_background');
+				break;
+			case 13:
+				$this->getCallbackClient()->jQuery($this->txt1, 'animate', array(
+					array(	'width' => '+=100',
+							'height' => '+=50'
+						),
+					array(
+							'duration' => 1000,
+						)
+					));
+				break;
+			case 14:
+				$this->getCallbackClient()->setAttribute($this->txt1, 'disabled', true);
+				break;
+			case 15:
+				$this->getCallbackClient()->setStyle($this->pan1, array('background-color' => 'blue'));
+				break;
+			case 16:
+				$this->getCallbackClient()->prependContent($this->pan1, 'prepend<br/>');
+				$this->getCallbackClient()->appendContent($this->pan1, '<br/>append');
+				break;
+			case 17:
+				$this->getCallbackClient()->insertContentBefore($this->pan1, 'before');
+				$this->getCallbackClient()->insertContentAfter($this->pan1, 'after');
+				break;
+			case 18:
+				$this->getCallbackClient()->replaceContent($this->pan1, 'No more Panel 1');
+				break;
+			case 19:
+				$this->getCallbackClient()->remove($this->txt1);
+				break;
+			case 20:
+				$this->getCallbackClient()->fadeOut($this->txt1);
+				break;
+			case 21:
+				$this->getCallbackClient()->fadeIn($this->txt1);
+				break;
+			case 22:
+				$this->getCallbackClient()->click($this->pan1);
+				// alternative
+				// $this->getCallbackClient()->raiseClientEvent($this->pan1, 'click');
+				break;
+			case 23:
+				$this->getCallbackClient()->jQuery($this->txt1, 'toggle');
+				break;
+
 		}
 	}
 }
