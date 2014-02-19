ae7a187ec50d2a4d68fc9b22acf7d46660eca402
Fabio Bas <ctrlaltca@gmail.com>
2014-02-19 19:27:30 +0100

    Update local tests path

--- tests/test_tools/PradoGenericSeleniumTest.php
+++ tests/test_tools/PradoGenericSeleniumTest.php
@@ -3,8 +3,8 @@ require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
 
 class PradoGenericSeleniumTest extends PHPUnit_Extensions_SeleniumTestCase
 {
-	static $browser='*googlechrome';
-	static $baseurl='http://127.0.0.1/prado-3.2/tests/FunctionalTests/';
+	static $browser='*firefox';
+	static $baseurl='http://127.0.0.1/prado-master/tests/FunctionalTests/';
 
 	protected function setUp()
 	{
