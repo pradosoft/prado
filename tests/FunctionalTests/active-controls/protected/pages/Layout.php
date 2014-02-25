a0d6864093cf13a96dafa8a47b5c43891b2b864f
Fabio Bas <ctrlaltca@gmail.com>
2014-02-25 22:43:00 +0100

    Ensure all tests have a proper doctype and html structure, to avoid browser's quirks mode

---
+++ tests/FunctionalTests/active-controls/protected/pages/Layout.php
@@ -0,0 +1,5 @@
+<?php
+
+class Layout extends TTemplateControl
+{
+}
--- tests/test_tools/PradoGenericSeleniumTest.php
+++ tests/test_tools/PradoGenericSeleniumTest.php
@@ -4,7 +4,7 @@ require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
 class PradoGenericSeleniumTest extends PHPUnit_Extensions_SeleniumTestCase
 {
 	public static $browsers = array(
-		/*
+/*
 		array(
 			'name'    => 'Firefox on OSX',
 			'browser' => '*firefox',
@@ -12,33 +12,35 @@ class PradoGenericSeleniumTest extends PHPUnit_Extensions_SeleniumTestCase
 			'port'    => 4444,
 			'timeout' => 30000,
 		),
+*/
 		array(
 			'name'    => 'Chrome on OSX',
-			'browser' => '*chrome',
+			'browser' => '*googlechrome',
 			'host'    => '127.0.0.1',
 			'port'    => 4444,
 			'timeout' => 30000,
 		),
+/*
 		array(
-			'name'    => 'Internet Explorer 11 on Windows8.1',
-			'browser' => '*iexplore',
-			'host'    => '192.168.253.39',
-			'port'    => 4444,
+			'name'    => 'Firefox on WindowsXP',
+			'browser' => '*firefox',
+			'host'    => '127.0.0.1',
+			'port'    => 4445,
 		),
-		*/
 		array(
 			'name'    => 'Internet Explorer 8 on WindowsXP',
 			'browser' => '*iehta',
 			'host'    => '127.0.0.1',
-			'port'    => 4444,
-		)
+			'port'    => 4445,
+		),
+*/
 	);
 
-	static $baseurl='http://10.0.2.2/prado-master/tests/FunctionalTests/';
+	static $baseurl='http://192.168.44.82/prado-master/tests/FunctionalTests/';
 
 	protected function setUp()
 	{
-//		$this->shareSession(true);
+		$this->shareSession(true);
 		$this->setBrowserUrl(static::$baseurl);
 	}
 
