6d626d0626bd0a0fe7053d09af5196f2ff796a07
David <ottodavid@gmx.net>
2014-07-30 19:35:41 +0200

    Fixed test case for issue 524 (cherry picked from commit 891ed122447ca88938b150da9858d6d4c271d020)

--- tests/FunctionalTests/issues/tests/Issue524TestCase.php
+++ tests/FunctionalTests/issues/tests/Issue524TestCase.php
@@ -14,7 +14,7 @@ class Issue524TestCase extends PradoGenericSelenium2Test
     $base='ctl0_Content_';
 
     $this->byID("{$base}buttonOk")->click();
-
+		$this->pause(800);
     $this->assertText("{$base}Validator", "fünf");
   }
 }
