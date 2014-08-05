99bed7d1149749d6cc96c6b5b87cbac8682753ce
David <ottodavid@gmx.net>
2014-08-05 23:02:07 +0200

    Fix System.Data test cases (cherry picked from commit c8682f71ca510b63d67bcf5c28e29992bfed94f2)

--- tests/unit/Data/TDbDataReaderTest.php
+++ tests/unit/Data/TDbDataReaderTest.php
@@ -37,6 +37,10 @@ class TDbDataReaderTest extends PHPUnit_Framework_TestCase
 	public function setUp()
 	{
 		@unlink(TEST_DB_FILE);
+
+		// create application just to provide application mode
+		new TApplication(__DIR__, false ,TApplication::CONFIG_TYPE_PHP);
+
 		$this->_connection=new TDbConnection('sqlite:'.TEST_DB_FILE);
 		$this->_connection->Active=true;
 		$this->_connection->createCommand('CREATE TABLE foo (id INTEGER NOT NULL PRIMARY KEY, name VARCHAR(8))')->execute();
