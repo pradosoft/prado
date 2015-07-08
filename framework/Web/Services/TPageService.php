0adeb70975cb1ecfcf2fe70576aedf33e0dc99bc
David <ottodavid@gmx.net>
2015-07-08 18:18:28 +0200

    Use Prado::createComponent instead of new for Pages
    This allows to use a custom Prado::createComponent as an hook for
    dependency injection containers

--- framework/Web/Services/TPageService.php
+++ framework/Web/Services/TPageService.php
@@ -501,7 +501,7 @@ class TPageService extends TService
  		if(!class_exists($className,false) || ($className!=='TPage' && !is_subclass_of($className,'TPage')))
 			throw new THttpException(404,'pageservice_page_unknown',$pagePath);
 
-		$page=new $className;
+		$page=Prado::createComponent($className);
 		$page->setPagePath($pagePath);
 
 		if($hasTemplateFile)
