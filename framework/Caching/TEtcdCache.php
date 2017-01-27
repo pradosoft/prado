7f75d8985a89d7502c459125a318e82e90c8b34a
LCSKJ <kj.landwehr.software@gmail.com>
2017-01-27 12:05:00 +0100

    Added two new cache modules based on key-value stores (Redis and etcd).

---
+++ framework/Caching/TEtcdCache.php
@@ -0,0 +1,223 @@
+<?php
+/**
+ * TEtcdCache class file
+ *
+ * @author LANDWEHR Computer und Software GmbH <programmierung@landwehr-software.de>
+ * @link https://github.com/pradosoft/prado4
+ * @copyright Copyright &copy; 2005-2016 The PRADO Group
+ * @license https://github.com/pradosoft/prado4/blob/master/LICENSE
+ * @package Prado\Caching
+ */
+
+namespace Prado\Caching;
+use Prado\TPropertyValue;
+use Prado\Exceptions\TConfigurationException;
+
+/**
+ * TEtcdCache class
+ *
+ * TEtcdCache implements a cache application module based on the distributed,
+ * consistent key-value store {@link https://github.com/coreos/etcd etcd}.
+ * etcd is high performance key-value store written in Go which uses the Raft
+ * consensus algorithm to manage a highly-available replicated log.
+ *
+ * By definition, cache does not ensure the existence of a value
+ * even if it never expires. Cache is not meant to be an persistent storage.
+ *
+ * To use this module, an etcd instance must be running and reachable on the host
+ * specified by {@link setHost} and the port specified by {@link setPort} which
+ * default to 'localhost:2379'. All values are stored within a directory set by
+ * {@link setDir} which defaults to 'pradocache'.
+ * 
+ * TEtcdCache only supports etcd API v2 and uses cURL to fire the HTTP
+ * GET/PUT/DELETE commands, thus the PHP cURL extension is also needed.
+ *
+ * Some usage examples of TEtcdCache are as follows,
+ * <code>
+ * $cache = new TEtcdCache(); // TEtcdCache may also be loaded as a Prado application module
+ * $cache->init(null);
+ * $cache->add('value', $value);
+ * $value = $cache->get('value');
+ * </code>
+ *
+ * If loaded, TEtcdCache will register itself with {@link TApplication} as the
+ * cache module. It can be accessed via {@link TApplication::getCache()}.
+ *
+ * TEtcdCache may be configured in application configuration file as follows
+ * <code>
+ * <module id="cache" class="System.Caching.TEtcdCache" Host="localhost" Port="2379" Dir="pradocache" />
+ * </code>
+ *
+ * @author LANDWEHR Computer und Software GmbH <programmierung@landwehr-software.de>
+ * @package Prado\Caching
+ * @since 4.0
+ */
+class TEtcdCache extends TCache
+{
+
+  /**
+   * @var string the etcd host
+   */
+  protected $_host = 'localhost';
+  
+  /**
+   * @var integer the etcd port
+   */
+  protected $_port = 2379;
+  
+  /**
+   * @var string the directory to store values in
+   */
+  protected $_dir = 'pradocache';
+
+   /**
+    * Initializes this module.
+    * This method is required by the IModule interface.
+    * @param TXmlElement configuration for this module, can be null
+    * @throws TConfigurationException if cURL extension is not installed
+    */
+	public function init($config)
+	{
+		if (!function_exists('curl_version')) throw new TConfigurationException('curl_extension_required');
+		parent::init($config);
+	}
+  
+  /**
+	 * Gets the host the etcd instance is running on, defaults to 'localhost'.
+	 * @return string the etcd host
+	 */
+	public function getHost() {
+	  return $this->_host;
+	}
+
+	/**
+	 * Sets the host the etcd instance is running on.
+	 * @param string $value the etcd host
+	 */
+	public function setHost($value) {
+    $this->_host = TPropertyValue::ensureString($value);
+	}
+    
+  /**
+	 * Gets the port the etcd instance is running on, defaults to 2379.
+	 * @return integer the etcd port
+	 */
+	public function getPort() {
+	  return $this->_port;
+	}
+
+	/**
+	 * Sets the port the etcd instance is running on.
+	 * @param integer $value the etcd port
+	 */
+	public function setPort($value) {
+    $this->_port = TPropertyValue::ensureInteger($value);
+	}
+     
+  /**
+	 * Sets the directory to store values in, defaults to 'pradocache'.
+	 * @return string the directory to store values in
+	 */
+	public function getDir() {
+	  return $this->_dir;
+	}
+
+	/**
+	 * Gets the directory to store values in.
+	 * @param string $value the directory to store values in
+	 */
+	public function setDir($value) {
+    $this->_dir = TPropertyValue::ensureString($value);
+	}
+
+	/**
+	 * Retrieves a value from cache with a specified key.
+	 * This is the implementation of the method declared in the parent class.
+	 * @param string a unique key identifying the cached value
+	 * @return string the value stored in cache, false if the value is not in the cache or expired.
+	 */
+	protected function getValue($key)
+	{                                     
+		$result = $this->request('GET', $this->_dir . '/' . $key);
+    return property_exists($result, 'errorCode') ? false : unserialize($result->node->value);
+	}
+
+	/**
+	 * Stores a value identified by a key in cache.
+	 * This is the implementation of the method declared in the parent class.
+	 *
+	 * @param string the key identifying the value to be cached
+	 * @param string the value to be cached
+	 * @param integer the number of seconds in which the cached value will expire. 0 means never expire.
+	 * @return boolean true if the value is successfully stored into cache, false otherwise
+	 */
+	protected function setValue($key, $value, $expire)
+	{
+    $value = array('value' => serialize($value));
+    if ($expire > 0) $value['ttl'] = $expire;
+		$result = $this->request('PUT', $this->_dir . '/' . $key, $value);    
+    return !property_exists($result, 'errorCode');
+	}
+
+	/**
+	 * Stores a value identified by a key into cache if the cache does not contain this key.
+	 * This is the implementation of the method declared in the parent class.
+	 *
+	 * @param string the key identifying the value to be cached
+	 * @param string the value to be cached
+	 * @param integer the number of seconds in which the cached value will expire. 0 means never expire.
+	 * @return boolean true if the value is successfully stored into cache, false otherwise
+	 */
+	protected function addValue($key, $value, $expire)
+	{
+    $value = array('value' => serialize($value), 'prevExist' => 'false');
+    if ($expire > 0) $value['ttl'] = $expire;
+		$result = $this->request('PUT', $this->_dir . '/' . $key, $value);
+    return !property_exists($result, 'errorCode');
+	}
+
+	/**
+	 * Deletes a value with the specified key from cache
+	 * This is the implementation of the method declared in the parent class.
+	 * @param string the key of the value to be deleted
+	 * @return boolean if no error happens during deletion
+	 */
+	protected function deleteValue($key)
+	{
+		$this->request('DELETE', $this->_dir . '/' . $key);
+    return true;
+	}
+
+	/**
+	 * Deletes all values from cache.
+	 * Be careful of performing this operation if the cache is shared by multiple applications.
+	 */
+	public function flush()
+	{
+     $this->request('DELETE', $this->_dir . '?recursive=true');
+	}
+  
+  /**
+	 * This method does the actual cURL request by generating the method specific
+	 * URL, setting the cURL options and adding additional request parameters.
+	 * The etcd always returns a JSON string which is decoded and returned to
+	 * the calling method.
+	 * @param string the HTTP method for the request (GET,PUT,DELETE)
+	 * @param string the the key to perform the action on (includes the directory)
+	 * @param array the additional post data to send with the request
+	 * @return \stdClass the response from the etcd instance
+	 */
+  protected function request($method, $key, $value=array()) {
+    $curl = curl_init("http://{$this->_host}:{$this->_port}/v2/keys/{$key}");
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);   
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($value));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($response);
+  }
+
+}
---
+++ framework/Caching/TRedisCache.php
@@ -0,0 +1,286 @@
+<?php
+/**
+ * TRedisCache class file
+ *
+ * @author Jens Klaer <kj.landwehr.software@gmail.com>
+ * @author LANDWEHR Computer und Software GmbH
+ * @link https://github.com/pradosoft/prado4
+ * @copyright Copyright &copy; 2005-2016 The PRADO Group
+ * @license https://github.com/pradosoft/prado4/blob/master/LICENSE
+ * @package Prado\Caching
+ */
+
+namespace Prado\Caching;
+use Prado\Exceptions\TConfigurationException;
+use Prado\Exceptions\TInvalidOperationException;
+use Prado\Prado;
+use Prado\TPropertyValue;
+use Prado\Xml\TXmlElement;
+
+/**
+ * TRedisCache class
+ *
+ * TRedisCache implements a cache application module based on {@link https://redis.io/ redis} key-value store.
+ *
+ * TRedisCache can be configured with the {@link setHost Host} and {@link setPort Port}
+ * properties, which specify the host and port of the redis server to be used.
+ * By default, they take the value 'localhost' and 6379, respectively.
+ *
+ * It is also possible to use a unix socket for connection, it can be set
+ * using {@link setSocket}. Be sure that the socket is readable/writeable by
+ * the webserver/php user. By default, this value is left empty. If both,
+ * server/port and socket are set, the latter takes precedence.
+ *
+ * Use the {@link setIndex Index} property to change the database to the given
+ * database index. Defaults to 0.
+ *
+ * The following basic cache operations are implemented:
+ * - {@link get} : retrieve the value with a key (if any) from cache
+ * - {@link set} : store the value with a key into cache
+ * - {@link add} : store the value only if cache does not have this key
+ * - {@link delete} : delete the value with the specified key from cache
+ * - {@link flush} : delete all values from cache
+ *
+ * Each value is associated with an expiration time. The {@link get} operation
+ * ensures that any expired value will not be returned. The expiration time can
+ * be specified by the number of seconds. A expiration time 0 represents never expire.
+ *
+ * By definition, cache does not ensure the existence of a value
+ * even if it never expires. Cache is not meant to be an persistent storage.
+ *
+ * Also note, there is no security measure to protected data in redis cache.
+ * All data in redis cache can be accessed by any process running in the system.
+ *
+ * To use this module, the php-redis extension must be loaded.
+ *
+ * Some usage examples of TRedisCache are as follows,
+ * <code>
+ * $cache=new TRedisCache;  // TRedisache may also be loaded as a Prado application module
+ * $cache->init(null);
+ * $cache->add('object',$object);
+ * $object2=$cache->get('object');
+ * </code>
+ *
+ * If loaded as module, TRedisCache will register itself with {@link TApplication} as the
+ * default cache module. It can be accessed via {@link TApplication::getCache()}.
+ *
+ * TRedisCache may be configured in application configuration file as follows
+ * <code>
+ * <module id="cache" class="System.Caching.TRedisCache" Host="localhost" Port="6379" />
+ * </code>
+ * or
+ * <code>
+ * <module id="cache" class="System.Caching.TRedisCache" Socket="var/run/redis/redis.sock" Index="2" />
+ * </code>
+ * where {@link setHost Host} and {@link setPort Port} or {@link setSocket Socket} are configurable properties
+ * of TRedisCache.
+
+ * @author Jens Klaer <kj.landwehr.software@gmail.com>
+ * @author LANDWEHR Computer und Software GmbH
+ * @since 4.0
+ */
+class TRedisCache extends TCache
+{
+  /**
+   * @var boolean if the module is initialized
+   */
+  private $_initialized = false;
+  /**
+   * @var \Redis the Redis instance
+   */
+  private $_cache = null;
+  /**
+   * @var string host name of the redis cache server
+   */
+  private $_host = 'localhost';
+  /**
+   * @var integer the port number of the redis cache server
+   */
+  private $_port = 6379;
+  /**
+   * @var string the unix socket of the redis cache server.
+   */
+  private $_socket = null;
+  /**
+   * @var integer the database index to use within the redis server.
+   */
+  private $_index = 0;
+
+  /**
+   * Destructor.
+   * Disconnect the redis cache server.
+   */
+  public function __destruct()
+  {
+    if ($this->_cache instanceof \Redis)
+      $this->_cache->close();
+  }
+
+  /**
+   * Initializes this module.
+   * This method is required by the IModule interface. It creates a Memcache instance and connects to the memcache server.
+   * @param TApplication Prado application, can be null
+   * @param TXmlElement configuration for this module, can be null
+   * @throws TConfigurationException if php-redis extension is not installed or redis cache sever connection fails
+   */
+  public function init($config)
+  {
+    if (!extension_loaded('redis') || !class_exists('\Redis', false))
+      throw new TConfigurationException('rediscache_extension_required');
+    $this->_cache = new \Redis();
+    if ($this->_socket !== null)
+      $this->_cache->connect($this->_socket);
+    else
+      $this->_cache->connect($this->_host, $this->_port);
+    $this->_cache->setOption(\Redis::OPT_SERIALIZER, \Redis::SERIALIZER_PHP);
+    $this->_cache->select($this->_index);
+    parent::init($config);
+    $this->_initialized = true;
+  }
+
+  public function valid($key) {
+    return true;
+  }
+
+  /**
+   * @return string the host name of the redis cache server
+   */
+  public function getHost()
+  {
+    return $this->_host;
+  }
+
+  /**
+   * @param string the host name of the redis cache server
+   * @throws TInvalidOperationException if the module is already initialized
+   */
+  public function setHost($value)
+  {
+    if ($this->_initialized)
+      throw new TInvalidOperationException('rediscache_host_unchangeable');
+    else
+      $this->_host = $value;
+  }
+
+  /**
+   * @return integer the port number of the redis cache server
+   */
+  public function getPort()
+  {
+    return $this->_port;
+  }
+
+  /**
+   * @param integer the port number of the redis cache server
+   * @throws TInvalidOperationException if the module is already initialized
+   */
+  public function setPort($value)
+  {
+    if ($this->_initialized)
+      throw new TInvalidOperationException('rediscache_port_unchangeable');
+    else
+      $this->_port = TPropertyValue::ensureInteger($value);
+  }
+
+  /**
+   * @return string the unix socket of the redis cache server
+   */
+  public function getSocket()
+  {
+    return $this->_socket;
+  }
+
+  /**
+   * @param string the unix socket of the redis cache server
+   * @throws TInvalidOperationException if the module is already initialized
+   */
+  public function setSocket($value)
+  {
+    if($this->_initialized)
+      throw new TInvalidOperationException('rediscache_socket_unchangeable');
+    else
+      $this->_socket = TPropertyValue::ensureString($value);
+  }
+
+  /**
+   * @return integer the database index to use. Defaults to 0.
+   */
+  public function getIndex()
+  {
+    return $this->_index;
+  }
+
+  /**
+   * @param integer the database index to use.
+   * @throws TInvalidOperationException if the module is already initialized
+   */
+  public function setIndex($value)
+  {
+    if ($this->_initialized)
+      throw new TInvalidOperationException('rediscache_index_unchangeable');
+    else
+      $this->_index = TPropertyValue::ensureInteger($value);
+  }
+
+  /**
+   * Retrieves a value from cache with a specified key.
+   * This is the implementation of the method declared in the parent class.
+   * @param string a unique key identifying the cached value
+   * @return string the value stored in cache, false if the value is not in the cache or expired.
+   */
+  protected function getValue($key)
+  {
+    return $this->_cache->get($key);
+  }
+
+  /**
+   * Stores a value identified by a key in cache.
+   * This is the implementation of the method declared in the parent class.
+   *
+   * @param string the key identifying the value to be cached
+   * @param string the value to be cached
+   * @param integer the number of seconds in which the cached value will expire. 0 means never expire.
+   * @return boolean true if the value is successfully stored into cache, false otherwise
+   */
+  protected function setValue($key, $value, $expire)
+  {
+    $options = $expire === 0 ? array() : array('ex' => $expire);
+    return $this->_cache->set($key, $value, $options);
+  }
+
+  /**
+   * Stores a value identified by a key into cache if the cache does not contain this key.
+   * This is the implementation of the method declared in the parent class.
+   *
+   * @param string the key identifying the value to be cached
+   * @param string the value to be cached
+   * @param integer the number of seconds in which the cached value will expire. 0 means never expire.
+   * @return boolean true if the value is successfully stored into cache, false otherwise
+   */
+  protected function addValue($key, $value, $expire)
+  {
+    $options = $expire === 0 ? array('nx') : array('nx', 'ex' => $expire);
+    return $this->_cache->set($key, $value, $options);
+  }
+
+  /**
+   * Deletes a value with the specified key from cache
+   * This is the implementation of the method declared in the parent class.
+   * @param string the key of the value to be deleted
+   * @return boolean if no error happens during deletion
+   */
+  protected function deleteValue($key)
+  {
+    $this->_cache->delete($key);
+    return true;
+  }
+
+  /**
+   * Deletes all values from cache, only clearing the currently selected database.
+   */
+  public function flush()
+  {
+    return $this->_cache->flushDB();
+  }
+}
+
