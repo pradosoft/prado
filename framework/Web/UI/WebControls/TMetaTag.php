1c48df73bb2716706c37e5476bbef42005b3ea7d
frkinta <frkinta@gmail.com>
2018-02-04 18:15:47 +0100

    Update TMetaTag.php

--- framework/Web/UI/WebControls/TMetaTag.php
+++ framework/Web/UI/WebControls/TMetaTag.php
@@ -16,9 +16,9 @@ namespace Prado\Web\UI\WebControls;
  *
  * TMetaTag represents a meta tag appearing in a page head section.
  * You can set its {@link setID ID}, {@link setHttpEquiv HttpEquiv},
- * {@link setName Name}, {@link setContent Content}, {@link setScheme Scheme}
- * , {@link setCharset Charset} properties, which correspond to 
- * id, http-equiv, name, content, and scheme  * attributes for a meta tag, respectively.
+ * {@link setName Name}, {@link setContent Content}, {@link setScheme Scheme}, {@link setCharset Charset}
+ * properties, which correspond to 
+ * id, http-equiv, name, content, scheme and charset * attributes for a meta tag, respectively.
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @package Prado\Web\UI\WebControls
@@ -163,8 +163,8 @@ class TMetaTag extends \Prado\TComponent
 			$writer->addAttribute('scheme',$this->_scheme);
 		if($this->_charset!=='')
 			$writer->addAttribute('charset',$this->_charset);
-		      if ($this->_charset === '')
-		$writer->addAttribute('content',$this->_content);
+		if ($this->_charset === '')
+			$writer->addAttribute('content',$this->_content);
 		$writer->renderBeginTag('meta');
 		$writer->renderEndTag();
 	}
