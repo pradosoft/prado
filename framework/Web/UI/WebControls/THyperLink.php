pradosoft/prado

commit a2b724fc4ffddd279c98ba1fe9e738aa95a4b1e6

THyperlink: add ImageStyle property to replace the deprecated ImageAlign, ImageWidth and ImageHeight properties
Also, fix the documentation about these deprecated properties

framework/Web/UI/WebControls/THyperLink.php
@@ -20,8 +20,8 @@ use Prado\Web\THttpUtility;
  * via the {@link setNavigateUrl NavigateUrl} property, and link text is via
  * the {@link setText Text} property. It is also possible to display an image
  * by setting the {@link setImageUrl ImageUrl} property. In this case,
- * the alignment of the image displayed is set by the
- * {@link setImageAlign ImageAlign} property and {@link getText Text} is
+ * the style of the image displayed can be set using the
+ * {@link setImageStyle ImageStyle} property and {@link getText Text} is
  * displayed as the alternate text of the image.
  *
  * The link target is specified via the {@link setTarget Target} property.
@@ -90,22 +90,15 @@ class THyperLink extends \Prado\Web\UI\WebControls\TWebControl implements \Prado
 	{
 		$image = new TImage;
 		$image->setImageUrl($imageUrl);
-		if (($width = $this->getImageWidth()) !== '') {
-			$image->setWidth($width);
-		}
-		if (($height = $this->getImageHeight()) !== '') {
-			$image->setHeight($height);
-		}
 		if (($toolTip = $this->getToolTip()) !== '') {
 			$image->setToolTip($toolTip);
 		}
 		if (($text = $this->getText()) !== '') {
 			$image->setAlternateText($text);
 		}
-		if (($align = $this->getImageAlign()) !== '') {
-			$image->setImageAlign($align);
+		if (($style = $this->getViewState('ImageStyle', null)) !== null) {
+			$image->getStyle()->copyFrom($style);
 		}
-		$image->setBorderWidth('0');
 		return $image;
 	}
 
@@ -128,10 +121,11 @@ class THyperLink extends \Prado\Web\UI\WebControls\TWebControl implements \Prado
 
 	/**
 	 * @return string the alignment of the image with respective to other elements on the page, defaults to empty.
+	 * @deprecated use the ImageStyle property to get the float and/or vertical-align CSS properties instead
 	 */
 	public function getImageAlign()
 	{
-		return $this->getViewState('ImageAlign', '');
+		return $this->getImageStyle()->getImageAlign();
 	}
 
 	/**
@@ -140,27 +134,30 @@ class THyperLink extends \Prado\Web\UI\WebControls\TWebControl implements \Prado
 	 * middle, right, texttop, and top. If an empty string is passed in,
 	 * imagealign attribute will not be rendered.
 	 * @param string $value the alignment of the image
+	 * @deprecated use the ImageStyle property to set the float and/or vertical-align CSS properties instead
 	 */
 	public function setImageAlign($value)
 	{
-		$this->setViewState('ImageAlign', $value, '');
+		$this->getImageStyle()->setImageAlign($value);
 	}
 
 	/**
 	 * @return string height of the image in the THyperLink
+	 * @deprecated use the ImageStyle.Height property to get the height instead
 	 */
 	public function getImageHeight()
 	{
-		return $this->getViewState('ImageHeight', '');
+		return $this->getImageStyle()->getHeight();
 	}
 
 	/**
 	 * Sets the height of the image in the THyperLink
 	 * @param string $value height of the image in the THyperLink
+	 * @deprecated use the ImageStyle property to set the height CSS property instead
 	 */
 	public function setImageHeight($value)
 	{
-		$this->setViewSTate('ImageHeight', $value, '');
+		$this->getImageStyle()->setHeight($value);
 	}
 
 	/**
@@ -182,19 +179,33 @@ class THyperLink extends \Prado\Web\UI\WebControls\TWebControl implements \Prado
 
 	/**
 	 * @return string width of the image in the THyperLink
+	 * @deprecated use the ImageStyle.Width property to get the width property instead
 	 */
 	public function getImageWidth()
 	{
-		return $this->getViewState('ImageWidth', '');
+		return $this->getImageStyle()->getWidth();
 	}
 
 	/**
 	 * Sets the width of the image in the THyperLink
 	 * @param string $value width of the image
+	 * @deprecated use the ImageStyle property to set the width CSS property instead
 	 */
 	public function setImageWidth($value)
 	{
-		$this->setViewState('ImageWidth', $value, '');
+		$this->getImageStyle()->setWidth($value);
+	}
+
+	/**
+	 * @return TStyle the style for the inner image
+	 */
+	public function getImageStyle()
+	{
+		if (($style = $this->getViewState('ImageStyle', null)) === null) {
+			$style = new TStyle;
+			$this->setViewState('ImageStyle', $style, null);
+		}
+		return $style;
 	}
 
 	/**

framework/Web/UI/ActiveControls/TActiveImage.php
@@ -16,9 +16,8 @@ use Prado\Web\UI\WebControls\TImage;
  * TActiveImage class.
  *
  * TActiveImage allows the {@link setAlternateText AlternateText},
- * {@link setImageAlign ImageAlign}, {@link setImageUrl ImageUrl},
- * and {@link setDescriptionUrl DescriptionUrl} to be updated during
- * a callback request.
+ * {@link setImageUrl ImageUrl}, and {@link setDescriptionUrl DescriptionUrl}
+ * to be updated during a callback request.
  *
  * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
  * @package Prado\Web\UI\ActiveControls
@@ -67,6 +66,7 @@ class TActiveImage extends TImage implements IActiveControl
 	 * middle, right, texttop, and top. If an empty string is passed in,
 	 * imagealign attribute will not be rendered.
 	 * @param string $value the alignment of the image
+	 * @deprecated use the Style property to set the float and/or vertical-align CSS properties instead
 	 */
 	public function setImageAlign($value)
 	{
@@ -97,6 +97,7 @@ class TActiveImage extends TImage implements IActiveControl
 
 	/**
 	 * @param string $value the URL to the long description of the image.
+	 * @deprecated use a WAI-ARIA alternative such as aria-describedby or aria-details instead.
 	 */
 	public function setDescriptionUrl($value)
 	{

framework/Web/UI/ActiveControls/TActiveImageButton.php
@@ -77,6 +77,7 @@ class TActiveImageButton extends TImageButton implements IActiveControl, ICallba
 	 * middle, right, texttop, and top. If an empty string is passed in,
 	 * imagealign attribute will not be rendered.
 	 * @param string $value the alignment of the image
+	 * @deprecated use the Style property to set the float and/or vertical-align CSS properties instead
 	 */
 	public function setImageAlign($value)
 	{
@@ -107,6 +108,7 @@ class TActiveImageButton extends TImageButton implements IActiveControl, ICallba
 
 	/**
 	 * @param string $value the URL to the long description of the image.
+	 * @deprecated use a WAI-ARIA alternative such as aria-describedby or aria-details instead.
 	 */
 	public function setDescriptionUrl($value)
 	{

framework/Web/UI/WebControls/TImage.php
@@ -15,10 +15,9 @@ namespace Prado\Web\UI\WebControls;
  *
  * TImage displays an image on a page. The image is specified via the
  * {@link setImageUrl ImageUrl} property which takes a relative or absolute
- * URL to the image file. The alignment of the image displayed is set by
- * the {@link setImageAlign ImageAlign} property. To set alternative texts
- * or long description of the image, use {@link setAlternateText AlternateText}
- * or {@link setDescriptionUrl DescriptionUrl} property, respectively.
+ * URL to the image file. The style of the image displayed can be set using
+ * the {@link setStyle Style} property. To set an alternative text
+ * use the {@link setAlternateText AlternateText} property.
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @package Prado\Web\UI\WebControls
@@ -79,7 +78,7 @@ class TImage extends \Prado\Web\UI\WebControls\TWebControl implements \Prado\IDa
 
 	/**
 	 * @return string the alignment of the image with respective to other elements on the page, defaults to empty.
-	 * @deprecated use the float and/or vertical-align CSS properties instead
+	 * @deprecated use the Style property to get the float and/or vertical-align CSS properties instead
 	 */
 	public function getImageAlign()
 	{
@@ -92,7 +91,7 @@ class TImage extends \Prado\Web\UI\WebControls\TWebControl implements \Prado\IDa
 	 * middle, right, texttop, and top. If an empty string is passed in,
 	 * imagealign attribute will not be rendered.
 	 * @param string $value the alignment of the image
-	 * @deprecated use the float and/or vertical-align CSS properties instead
+	 * @deprecated use the Style property to set the float and/or vertical-align CSS properties instead
 	 */
 	public function setImageAlign($value)
 	{

commit 125210fdaad32259901f8aebfafe920cddc848de

THyperLink: fix ImageAlign broken in a2b724fc4ffddd279c98ba1fe9e738aa95a4b1e6

framework/Web/UI/WebControls/THyperLink.php
@@ -99,6 +99,9 @@ class THyperLink extends \Prado\Web\UI\WebControls\TWebControl implements \Prado
 		if (($style = $this->getViewState('ImageStyle', null)) !== null) {
 			$image->getStyle()->copyFrom($style);
 		}
+		if (($align = $this->getImageAlign()) !== '') {
+			$image->setImageAlign($align);
+		}
 		return $image;
 	}
 
@@ -125,7 +128,7 @@ class THyperLink extends \Prado\Web\UI\WebControls\TWebControl implements \Prado
 	 */
 	public function getImageAlign()
 	{
-		return $this->getImageStyle()->getImageAlign();
+		return $this->getViewState('ImageAlign', '');
 	}
 
 	/**
@@ -138,7 +141,7 @@ class THyperLink extends \Prado\Web\UI\WebControls\TWebControl implements \Prado
 	 */
 	public function setImageAlign($value)
 	{
-		$this->getImageStyle()->setImageAlign($value);
+		$this->setViewState('ImageAlign', $value, '');
 	}
 
 	/**