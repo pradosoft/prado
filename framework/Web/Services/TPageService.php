63930fb957255202aabbd3ee380f2e9687f977db
Belisoful <belisoful@icloud.com>
2021-04-20 00:21:46 -0700

    TPageService phpdoc format update

--- framework/Web/Services/TPageService.php
+++ framework/Web/Services/TPageService.php
@@ -533,7 +533,7 @@ class TPageService extends \Prado\TService
 	/**
 	 * This event is raised just before the page is run.  Any part of the system can  patch into the page
 	 * events with an onPreRunPage handler.
-	 * @param mixed what is passed as the parameter to the event
+	 * @param mixed $param what is passed as the parameter to the event
 	 */
 	public function onPreRunPage($param)
 	{
