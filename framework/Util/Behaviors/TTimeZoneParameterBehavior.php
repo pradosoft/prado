76a1fb3df186a1c37cd4b437122a1a9403bfd6e0
Belisoful <belisoful@icloud.com>
2021-04-21 12:32:21 -0700

    Put the TimeZoneParameterBehavior Back with a proper TimeZone name

---
+++ framework/Util/Behaviors/TTimeZoneParameterBehavior.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * TTimeZoneParameterBehavior class file.
+ *
+ * @author Brad Anderson <belisoful@icloud.com>
+ * @link https://github.com/pradosoft/prado
+ * @license https://github.com/pradosoft/prado/blob/master/LICENSE
+ */
+
+namespace Prado\Util\Behaviors;
+
+use Prado\Prado;
+use Prado\Util\TBehavior;
+
+/**
+ * TTimeZoneParameterBehavior sets the date_default_timezone_set.
+ * This parameterizes the TimeZone.   {@link TimeZoneParameter} is
+ * the key to the Application Parameter for setting the TimeZone.
+ *
+ * This Behavior is designed to attach to TApplication, but can be
+ * attached to any TComponent.
+ *
+ * <code>
+ *		<behavior name="TimeZoneParameter" Class="Prado\Util\Behaviors\TTimeZoneParameterBehavior" AttachTo="Application" TimeZoneParameter="TimeZone" TimeZone="America/New_York"/>
+ * </code>
+ * This code will set the default timeZone to "America/New_York", and then
+ * if there is any Application Parameter in "TimeZone", then that takes
+ * precedence.  Setting the TimeZoneParameter to "" will disable the
+ * parameter functionality and set the TimeZone from the attribute TimeZone.
+ *
+ * This routes changes in the Application Parameter {@link TimeZoneParameter}
+ * to {@link setTimeZone}.
+ *
+ * @author Brad Anderson <belisoful@icloud.com>
+ * @package Prado\Util\Behaviors
+ * @since 4.2.0
+ */
+class TTimeZoneParameterBehavior extends TBehavior
+{
+	/**
+	 * Name of the Application Parameter Routing Behavior
+	 */
+	const APP_PARAM_ROUTE_BEHAVIOR_NAME = 'TimeZoneParameter';
+	
+	/**
+	 * Default TimeZoneParameter
+	 */
+	const TIMEZONE_PARAMETER_NAME = 'prop:TimeZone';
+	
+	/**
+	 * @var string the page theme is set to this parameter key
+	 */
+	private $_timeZoneParameter = self::TIMEZONE_PARAMETER_NAME;
+	
+	/**
+	 * @var object {@link TMapRouteBehavior} that routes changes to the parameter
+	 * is handled by setTimeZone.
+	 */
+	private $_paramBehavior;
+	
+	/**
+	 * This sets the date_default_timezone_set with the value of the TimeZoneParameter
+	 * in the application parameters.  It attaches the Application Parameter handler behavior.
+	 * @param $owner object the object that this behavior is attached to.
+	 */
+	public function attach($owner)
+	{
+		parent::attach($owner);
+		if (!$this->_timeZoneParameter) {
+			return;
+		}
+		$appParams = Prado::getApplication()->getParameters();
+		if ($default_timezone = $appParams->itemAt($this->_timeZoneParameter)) {
+			$this->setTimeZone($default_timezone);
+		}
+		$this->_paramBehavior = new TMapRouteBehavior($this->_timeZoneParameter, [$this, 'setTimeZone']);
+		$appParams->attachBehavior(self::APP_PARAM_ROUTE_BEHAVIOR_NAME, $this->_paramBehavior);
+	}
+	
+	/**
+	 * This removes the Application Parameter handler behavior
+	 * @param $owner object the object that this behavior is attached to.
+	 */
+	public function detach($owner)
+	{
+		if ($this->_paramBehavior) {
+			Prado::getApplication()->getParameters()->detachBehavior(self::APP_PARAM_ROUTE_BEHAVIOR_NAME);
+		}
+		parent::detach($owner);
+	}
+	
+	/**
+	 * @return string Application parameter key to set the php TimeZone.
+	 */
+	public function getTimeZoneParameter()
+	{
+		return $this->_timeZoneParameter;
+	}
+	
+	/**
+	 * @param $value string Application parameter key to set the php TimeZone.
+	 */
+	public function setTimeZoneParameter($value)
+	{
+		if ($this->_paramBehavior) {
+			$this->_paramBehavior->_parameter = $value;
+		}
+		$this->_timeZoneParameter = $value;
+	}
+	
+	/**
+	 * @return string the timeZone from date_default_timezone_get.
+	 */
+	public function getTimeZone()
+	{
+		return date_default_timezone_get();
+	}
+	
+	/**
+	 * @param $value string passthrough to date_default_timezone_set
+	 */
+	public function setTimeZone($value)
+	{
+		try {
+			date_default_timezone_set($value);
+		} catch (\Exception $e) {
+		}
+	}
+}
