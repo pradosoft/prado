433b6367f7a121a0c7fdc4ce5972f053747d170e
Belisoful <belisoful@icloud.com>
2021-04-23 00:49:54 -0700

    TGlobalization typo correction

--- framework/I18N/TGlobalization.php
+++ framework/I18N/TGlobalization.php
@@ -314,7 +314,7 @@ class TGlobalization extends \Prado\TModule
 	/**
 	 * This returns whether or not the current culture or the specified
 	 * culture is right to left orientation.  Default $culture is null, which
-	 * equal to using  $this->Culture as the parameter.
+	 * equal to using $this->Culture as the parameter.
 	 * @param null|string $culture, default null which makes this grab the current object culture
 	 * @return bool
 	 */
