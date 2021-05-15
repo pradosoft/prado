84a8a108802416d7d708278354dfb07e9b7b3902
Belisoful <belisoful@icloud.com>
2021-05-15 12:26:53 -0700

    TMap_Behaviors utilities for handling TMap dyNoItem, dyAddItem, and dyRemoveItem

---
+++ framework/Util/Behaviors/TMapLazyLoadBehavior.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * TMapLazyLoadBehavior class file.
+ *
+ * @author Brad Anderson <belisoful@icloud.com>
+ * @link https://github.com/pradosoft/prado
+ * @license https://github.com/pradosoft/prado/blob/master/LICENSE
+ */
+
+namespace Prado\Util\Behaviors;
+
+use Prado\Util\TBehavior;
+use Prado\Exceptions\TInvalidDataTypeException;
+
+/**
+ * TMapLazyLoadBehavior class.
+ *
+ * TMapLazyLoadBehavior handles Application Parameters when there is
+ * no parameter key found.  This allows for lazy loading of the parameter.
+ * <code>
+ *		Prado::getApplication()->getParameters()->attachBehavior('name'
+ *			new TMapLazyLoadBehavior([$obj, 'getParam']));
+ * </code>
+ * This behavior will call $obj->getParam($key) every time the key
+ * is not found in the TMap or TPriorityMap.
+ *
+ * @author Brad Anderson <belisoful@icloud.com>
+ * @package Prado\Util\Behaviors
+ * @since 4.2.0
+ */
+class TMapLazyLoadBehavior extends TBehavior
+{
+	/**
+	 * @var callable the parameter to check for when there are changes
+	 */
+	private $_handler;
+	
+	/**
+	 * @param $handler callable the handler for setting the parameter
+	 */
+	public function __construct($handler)
+	{
+		if (!is_callable($handler)) {
+			throw new TInvalidDataTypeException('maplazyloadbehavior_handler_not_callable');
+		}
+		$this->_handler = $handler;
+		parent::__construct();
+	}
+	
+	/**
+	 * This is the dynamic event for handling TMap dyAddItem.
+	 * This calls handler($key).
+	 * @param $value mixed the value of the item being added
+	 * @param $key string the key of the item being added
+	 * @param $callchain {@link TCallChain} of event handlers
+	 * @return mixed returns the argv[0], chained to all handlers
+	 */
+	public function dyNoItem($value, $key, $callchain)
+	{
+		if (($nvalue = call_user_func($this->_handler, $key)) !== null) {
+			$value = $nvalue;
+		}
+		return $callchain->dyNoItem($value, $key);
+	}
+}
