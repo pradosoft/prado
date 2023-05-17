60d62cad9f0d326fea60ea6badd8506e53f50828
Belisoful <belisoful@icloud.com>
2023-05-17 00:10:06 -0700

    removed cleanup that doesn't need to happen for Debug mode

--- tests/unit/Data/SqlMap/Ticket589Test.php
+++ tests/unit/Data/SqlMap/Ticket589Test.php
@@ -5,17 +5,6 @@ use Prado\TApplicationComponent;
 
 class Ticket589Test extends PHPUnit\Framework\TestCase
 {
-	
-	
-	// ****   Delete the fxevent.cache file from testing.   ****
-	//  If there are further tests, this should be moved to the last test using this application.
-	protected function tearDown(): void
-	{
-		$app = Prado::getApplication();
-		if ($app !== null) {
-			unlink($app->getRuntimePath() . DIRECTORY_SEPARATOR . TApplicationComponent::FX_CACHE_FILE);
-		}
-	}
 	public function test()
 	{
 		$manager = new TSqlMapManager();
