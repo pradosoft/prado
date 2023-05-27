b09e5aa4d6b5d31e78511d1650baa71dbec31a2e
Belisoful <belisoful@icloud.com>
2023-05-27 04:31:15 -0700

    added @since to the new TMap methods

--- framework/Collections/TMap.php
+++ framework/Collections/TMap.php
@@ -193,6 +193,7 @@ class TMap extends \Prado\TComponent implements \IteratorAggregate, \ArrayAccess
 	 * @param mixed $item the item to be removed
 	 * @throws TInvalidOperationException if the map is read-only
 	 * @return array The array of keys and the item removed.
+	 * since 4.2.3
 	 */
 	public function removeItem(mixed $item): array
 	{
@@ -232,6 +233,7 @@ class TMap extends \Prado\TComponent implements \IteratorAggregate, \ArrayAccess
 	 * @param mixed $item the item
 	 * @param bool $multiple Return an array of all the keys. Default true.
 	 * @return false|mixed the key of the item in the map, false if not found.
+	 * since 4.2.3
 	 */
 	public function keyOf($item, bool $multiple = true): mixed
 	{
