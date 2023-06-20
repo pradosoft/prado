86ee42c83fcd09e1c92bea01baa9b2a3e91baebf
Belisoful <belisoful@icloud.com>
2023-06-20 03:29:03 -0700

    TComponent upgrade: raiseEvents in reverse [option], search for behavior by class
    There are 5 upgrades to TComponent:
    - __destruct ordering is corrected so behaviors are removed before unlisten. this is to properly unwind and reverses the order of __construct.
    - raiseEvent can execute the handlers in reverse.  This is needed if the first handlers needs to be executed last for proper priority/importance of the result.   This adds TEventResults::EVENT_REVERSE
    - asA is updated so that if a behavior name is not found it, and the $behaviorname is a class/interface, it will look for the first behavior instanceof
    - isa is updated to include traits so traits can properly have global class behaviors.
    -getBehaviors is updated to return all behaviors of a select class if a class is provided
    
    this also updates and refactors the TComponent unit tests to not use behavior classes as behavior names.

--- framework/TEventResults.php
+++ framework/TEventResults.php
@@ -5,7 +5,7 @@
  * @author Qiang Xue <qiang.xue@gmail.com>
  *
  * Global Events, intra-object events, Class behaviors, expanded behaviors
- * @author Brad Anderson <javalizard@mac.com>
+ * @author Brad Anderson <belisoful@icloud.com>
  *
  * @link https://github.com/pradosoft/prado
  * @license https://github.com/pradosoft/prado/blob/master/LICENSE
@@ -16,11 +16,12 @@ namespace Prado;
 /**
  * TEventResults class
  *
- * @author Brad Anderson <javalizard@mac.com>
+ * @author Brad Anderson <belisoful@icloud.com>
  */
 class TEventResults extends \Prado\TEnumerable
 {
 	public const EVENT_RESULT_FEED_FORWARD = 1;
 	public const EVENT_RESULT_FILTER = 2;
 	public const EVENT_RESULT_ALL = 4;
+	public const EVENT_REVERSE = 8;
 }
