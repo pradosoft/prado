a8da1993c9f59453b8641fa0f93bf379aeadcc3e
Belisoful <belisoful@icloud.com>
2026-03-30 15:09:16 -0700

    phpunit bootstrap constructs a TApplication #1020 (#1021)

--- tests/test_tools/phpunit_bootstrap.php
+++ tests/test_tools/phpunit_bootstrap.php
@@ -9,6 +9,14 @@
 require_once(__DIR__ . '/../../vendor/autoload.php');
 require_once(__DIR__ . '/../../framework/Prado.php');
 
+//  Use the test app at 'Security/app'
+$relativeAppPath = '/../unit/Security/app';
+$appPath = realpath(__DIR__ . DIRECTORY_SEPARATOR . $relativeAppPath);
+
+// For unit tests requiring a global TApplication object, 
+//  construct -which sets {@see Prado::getApplication()}- but do not run
+$application = new \Prado\TApplication($appPath);
+
 // for FunctionalTests
 require_once(__DIR__ . '/PradoGenericSelenium2Test.php');
 require_once(__DIR__ . '/PradoTestListener.php');
