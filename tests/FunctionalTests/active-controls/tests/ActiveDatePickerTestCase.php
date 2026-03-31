bceeb1147f220814d64d4133a5abf1abb109bfca
Belisoful <belisoful@icloud.com>
2026-03-31 07:31:47 +0000

    remove the days in month, not needed.

--- tests/FunctionalTests/active-controls/tests/ActiveDatePickerTestCase.php
+++ tests/FunctionalTests/active-controls/tests/ActiveDatePickerTestCase.php
@@ -2,11 +2,6 @@
 
 class ActiveDatePickerTestCase extends \Prado\Tests\PradoGenericSelenium2Test
 {
-	public function days_in_month($month, $year)
-	{
-		// calculate number of days in a month
-		return $month == 2 ? ($year % 4 ? 28 : ($year % 100 ? 29 : ($year % 400 ? 28 : 29))) : (($month - 1) % 7 % 2 ? 30 : 31);
-	}
 	public function test()
 	{
 		$base = 'ctl0_Content_';
