b7f6b7557bb016812f3a8e3df3e8c993d2ce6321
Belisoful <belisoful@icloud.com>
2026-04-25 13:25:45 -0700

    TEnumerable update (#1073)
    * TEnumerable adds hasConstant and valueOf helpers. unit tests.
    
    * TEnumerable returns null for unfound constants in valueOf rather than false.
    adds constantOf to reverse lookup the constant name from the value, when different. eg TWebColors.
    Better documentation.
    
    * class docblock update for TEnumerable::constantOf
    
    * doc block tweak.
    
    * revert for another PR

--- framework/TEnumerable.php
+++ framework/TEnumerable.php
@@ -12,58 +12,205 @@ namespace Prado;
 
 /**
  * TEnumerable class.
+ *
  * TEnumerable is the base class for all enumerable types.
- * To define an enumerable type, extend TEnumberable and define string constants.
+ * To define an enumerable type, extend TEnumerable and define string constants.
  * Each constant represents an enumerable value.
- * The constant name must be the same as the constant value.
+ * The constant name should usually be the same as the constant value.
  * For example,
  * ```php
  * class TTextAlign extends \Prado\TEnumerable
  * {
- *     const Left='Left';
- *     const Right='Right';
+ *     const Left = 'Left';
+ *     const Right = 'Right';
  * }
  * ```
  * Then, one can use the enumerable values such as TTextAlign::Left and
  * TTextAlign::Right.
  *
+ * As of 4.3.3, to access a constant value by variable, check its presence,
+ * or get the constant name by value, use {@see valueOf()}, {@see hasConstant()},
+ * and {@see constantOf()}.  For example:
+ * ```php
+ * $alignConstant = 'Left';
+ * $value = TTextAlign::valueOf($alignConstant);  // returns 'Left'
+ * $constant = TTextAlign::constantOf($value);    // returns 'Left'
+ * $constant === $alignConstant;                  // true
+ * $hasLeft = TTextAlign::hasConstant($alignConstant);  // true
+ * ```
+ *
  * @author Qiang Xue <qiang.xue@gmail.com>
+ * @author Brad Anderson <belisoful@icloud.com>
  * @since 3.0
  */
 class TEnumerable implements \Iterator
 {
+	private static $_types = [];
 	private $_enums = [];
 
+	/**
+	 * Constructor.
+	 *
+	 * Initializes the enumerable constants from the class definition.
+	 */
 	public function __construct()
 	{
-		$reflection = new \ReflectionClass($this);
+		$reflection = self::getReflectionClass();
 		$this->_enums = $reflection->getConstants();
 	}
 
+	/**
+	 * Returns the current enumerable value.
+	 *
+	 * @return mixed The current value.
+	 */
 	#[\ReturnTypeWillChange]
 	public function current()
 	{
 		return current($this->_enums);
 	}
 
+	/**
+	 * Returns the key of the current enumerable value.
+	 *
+	 * @return mixed The current key.
+	 */
 	#[\ReturnTypeWillChange]
 	public function key()
 	{
 		return key($this->_enums);
 	}
 
+	/**
+	 * Advances the internal pointer to the next element.
+	 */
 	public function next(): void
 	{
 		next($this->_enums);
 	}
 
+	/**
+	 * Rewinds the internal pointer to the first element.
+	 */
 	public function rewind(): void
 	{
 		reset($this->_enums);
 	}
 
+	/**
+	 * Checks if the current position is valid.
+	 *
+	 * @return bool True if the current position is valid, false otherwise.
+	 */
 	public function valid(): bool
 	{
 		return $this->current() !== false;
+	}
+
+	/**
+	 * Check if a constant exists in this enumerable type. The second parameter
+	 * is case sensitivity, defaulting to true. This is similar behavior to
+	 * {@see \ReflectionClass}. For example:
+	 * ```php
+	 * $has_Left = TTextAlign::valueOf('Left');  // true
+	 * $hasNo_left = TTextAlign::hasConstant('left');  // false
+	 * $hasNo_left = TTextAlign::hasConstant('left', true);  // false
+	 * $has_left = TTextAlign::valueOf('left', false);  // true
+	 * ```
+	 * Case Insensitivity iterates through all constants.
+	 * @param string $constant The constant name to check.
+	 * @param bool $caseSensitive Whether to perform case-sensitive check. Default is true.
+	 * @return bool True if the constant exists, false otherwise.
+	 */
+	public static function hasConstant($constant, bool $caseSensitive = true): bool
+	{
+		$ref = self::getReflectionClass();
+		if ($ref->hasConstant($constant)) {
+			return true;
+		}
+		if (!$caseSensitive) {
+			$consts = $ref->getConstants();
+			foreach ($consts as $name => $value) {
+				if (strcasecmp($name, $constant) === 0) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Gets the constant value by name. The second parameter is case sensitivity,
+	 * defaulting to true. This is similar behavior to {@see \ReflectionClass}.
+	 * For example:
+	 * ```php
+	 * $value_Left = TTextAlign::valueOf('Left');  // returns 'Left'
+	 * $null = TTextAlign::valueOf('left');  // returns null
+	 * $null = TTextAlign::valueOf('left', true);  // returns null
+	 * $value_left = TTextAlign::valueOf('left', false);  // returns 'Left'
+	 * ```
+	 * Case Insensitivity iterates through all constants.
+	 * @param string $constant The constant name to get.
+	 * @param bool $caseSensitive Whether to perform case-sensitive check. Default is true.
+	 * @return ?string The constant value or null if not found.
+	 */
+	public static function valueOf($constant, bool $caseSensitive = true): ?string
+	{
+		$ref = self::getReflectionClass();
+		if (($value = $ref->getConstant($constant)) !== false) {
+			return $value;
+		}
+		if (!$caseSensitive) {
+			$consts = $ref->getConstants();
+			foreach ($consts as $name => $value) {
+				if (strcasecmp($name, $constant) === 0) {
+					return $value;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Gets the constant name by value. The second parameter is case sensitivity,
+	 * defaulting to true. This is similar behavior to {@see \ReflectionClass}.
+	 * For example:
+	 * ```php
+	 * $constant_Left = TTextAlign::constantOf('Left');  // returns 'Left'
+	 * $null = TTextAlign::constantOf('left');  // returns null
+	 * $null = TTextAlign::constantOf('left', true);  // returns null
+	 * $constant_left = TTextAlign::constantOf('left', false);  // returns 'Left'
+	 * ```
+	 * Case Insensitivity iterates through all constants.
+	 * @param string $value The constant value to find.
+	 * @param bool $caseSensitive Whether to perform case-sensitive check. Default is true.
+	 * @return ?string The constant name or null if not found.
+	 */
+	public static function constantOf($value, bool $caseSensitive = true): ?string
+	{
+		$cmp = $caseSensitive ? '\strcmp' : '\strcasecmp';
+		$ref = self::getReflectionClass();
+		foreach ($ref->getConstants() as $constant => $constValue) {
+			if ($cmp($value, $constValue) === 0) {
+				return $constant;
+			}
+		}
+		return null;
+	}
+
+	// ----- Private Helpers
+
+	/**
+	 * Gets or creates the ReflectionClass for the current enumerable class.
+	 * Uses a static cache to store ReflectionClass instances per class.
+	 * @return \ReflectionClass The reflection class instance.
+	 */
+	private static function getReflectionClass(): \ReflectionClass
+	{
+		$class = static::class;
+		if (!isset(self::$_types[$class])) {
+			self::$_types[$class] = new \ReflectionClass($class);
+		}
+		return self::$_types[$class];
 	}
 }
---
+++ tests/unit/TEnumerableTest.php
@@ -0,0 +1,244 @@
+<?php
+
+use Prado\TEnumerable;
+
+/**
+ */
+class TEnumerableTest extends PHPUnit\Framework\TestCase
+{
+	protected function setUp(): void
+	{
+	}
+
+
+	protected function tearDown(): void
+	{
+	}
+
+	private static function createEnumerableClass(array $constants): string
+	{
+		$consts = [];
+		foreach ($constants as $name => $value) {
+			$consts[] = "const $name = '$value';";
+		}
+		$className = 'TestEnum' . uniqid();
+		$code = "class $className extends \\Prado\\TEnumerable { " . implode(' ', $consts) . " }";
+		eval($code);
+		return $className;
+	}
+
+	public function testConstructor()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left1', 'Right' => 'Right2', 'Center' => 'Center3']);
+		$enum = new $className();
+
+		self::assertInstanceOf(TEnumerable::class, $enum);
+	}
+
+	public function testIterator()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left1', 'Right' => 'Right2']);
+		$enum = new $className();
+
+		$values = [];
+		foreach ($enum as $key => $value) {
+			$values[$key] = $value;
+		}
+
+		self::assertEquals(['Left' => 'Left1', 'Right' => 'Right2'], $values);
+	}
+
+	public function testIteratorRewind()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left1', 'Right' => 'Right2', 'Center' => 'Center3']);
+		$enum = new $className();
+
+		$firstPass = [];
+		foreach ($enum as $key => $value) {
+			$firstPass[$key] = $value;
+		}
+
+		$secondPass = [];
+		foreach ($enum as $key => $value) {
+			$secondPass[$key] = $value;
+		}
+
+		self::assertEquals($firstPass, $secondPass);
+	}
+
+	public function testIteratorWithEmptyConstants()
+	{
+		$className = $this->createEnumerableClass([]);
+		$enum = new $className();
+
+		$values = [];
+		foreach ($enum as $key => $value) {
+			$values[$key] = $value;
+		}
+
+		self::assertEquals([], $values);
+	}
+
+	public function testIteratorValid()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left1']);
+		$enum = new $className();
+
+		self::assertTrue($enum->valid());
+
+		while ($enum->valid()) {
+			$enum->next();
+		}
+
+		self::assertFalse($enum->valid());
+	}
+
+	public function testIteratorCurrent()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left1', 'Right' => 'Right2']);
+		$enum = new $className();
+
+		self::assertEquals('Left1', $enum->current());
+
+		$enum->next();
+		self::assertEquals('Right2', $enum->current());
+	}
+
+	public function testIteratorKey()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left', 'Right' => 'Right']);
+		$enum = new $className();
+
+		self::assertEquals('Left', $enum->key());
+
+		$enum->next();
+		self::assertEquals('Right', $enum->key());
+	}
+
+	public function testHasConstant()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left1', 'Right' => 'Right2']);
+
+		self::assertTrue($className::hasConstant('Left'));
+		self::assertTrue($className::hasConstant('Right'));
+		self::assertFalse($className::hasConstant('Center'));
+		self::assertFalse($className::hasConstant('left'));
+		self::assertFalse($className::hasConstant('LEFT'));
+	}
+
+	public function testHasConstantCaseInsensitive()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left', 'Right' => 'Right']);
+
+		self::assertTrue($className::hasConstant('Left', false));
+		self::assertTrue($className::hasConstant('left', false));
+		// Note: The $caseSensitive parameter exists but is not currently used by ReflectionClass::hasConstant
+		self::assertTrue($className::hasConstant('LEFT', false));
+	}
+
+	public function testHasConstantWithNonExistentClass()
+	{
+		self::assertFalse(TEnumerable::hasConstant('SomeConstant'));
+		self::assertFalse(TEnumerable::hasConstant('NonExistent'));
+	}
+
+	public function testValueOf()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left', 'Right' => 'Right']);
+
+		self::assertEquals('Left', $className::valueOf('Left'));
+		self::assertEquals('Right', $className::valueOf('Right'));
+		self::assertNull($className::valueOf('Center'));
+		self::assertNull($className::valueOf('left'));
+		self::assertNull($className::valueOf('LEFT'));
+	}
+
+	public function testValueOfCaseInsensitive()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left', 'Right' => 'Right']);
+
+		self::assertEquals('Left', $className::valueOf('Left', false));
+		self::assertEquals('Left', $className::valueOf('left', false));
+		self::assertEquals('Left', $className::valueOf('LEFT', false));
+		self::assertEquals('Right', $className::valueOf('RIGHT', false));
+	}
+
+	public function testValueOfNotFound()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left', 'Right' => 'Right']);
+
+		self::assertNull($className::valueOf('Center'));
+		self::assertNull($className::valueOf('Center', false));
+	}
+
+	public function testAccessConstantValueViaVariable()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left', 'Right' => 'Right']);
+
+		$align = 'Left';
+		$value = constant($className . '::' . $align);
+		self::assertEquals('Left', $value);
+
+		$align = 'Right';
+		$value = constant($className . '::' . $align);
+		self::assertEquals('Right', $value);
+	}
+
+	public function testAccessConstantValueViaReflection()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left', 'Right' => 'Right']);
+
+		$ref = new ReflectionClass($className);
+
+		$align = 'Left';
+		$value = $ref->getConstant($align);
+		self::assertEquals('Left', $value);
+
+		$align = 'Right';
+		$value = $ref->getConstant($align);
+		self::assertEquals('Right', $value);
+
+		$value = $ref->getConstant('Center');
+		self::assertFalse($value);
+	}
+
+	public function testConstantOf()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left', 'Right' => 'Right']);
+
+		self::assertEquals('Left', $className::constantOf('Left'));
+		self::assertEquals('Right', $className::constantOf('Right'));
+		self::assertNull($className::constantOf('Center'));
+		self::assertNull($className::constantOf('left'));
+		self::assertNull($className::constantOf('LEFT'));
+	}
+
+	public function testConstantOfCaseInsensitive()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left', 'Right' => 'Right']);
+
+		self::assertEquals('Left', $className::constantOf('Left', false));
+		self::assertEquals('Left', $className::constantOf('left', false));
+		self::assertEquals('Left', $className::constantOf('LEFT', false));
+		self::assertEquals('Right', $className::constantOf('RIGHT', false));
+	}
+
+	public function testConstantOfNotFound()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'Left', 'Right' => 'Right']);
+
+		self::assertNull($className::constantOf('Center'));
+		self::assertNull($className::constantOf('Center', false));
+	}
+
+	public function testConstantOfWithDifferentNameAndValue()
+	{
+		$className = $this->createEnumerableClass(['Left' => 'LeftValue', 'Right' => 'RightValue']);
+
+		self::assertEquals('Left', $className::constantOf('LeftValue'));
+		self::assertEquals('Right', $className::constantOf('RightValue'));
+		self::assertNull($className::constantOf('Left'));
+		self::assertNull($className::constantOf('leftvalue', true));
+		self::assertEquals('Left', $className::constantOf('leftvalue', false));
+	}
+}
