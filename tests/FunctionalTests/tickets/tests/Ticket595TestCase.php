df2ca1abde7ce6c4e71eedbf1f61b941345127ee
Belisoful <belisoful@icloud.com>
2026-05-09 05:49:32 +0000

    Ticket 595 - TBaseValidator::updateControlCssClass better CssClass processing

--- tests/FunctionalTests/tickets/tests/Ticket595TestCase.php
+++ tests/FunctionalTests/tickets/tests/Ticket595TestCase.php
@@ -20,11 +20,11 @@ class Ticket595TestCase extends \Prado\Tests\PradoGenericSelenium2Test
 		$this->assertAttribute($base . 'A@class', '');
 
 		$this->click($base . 'ctl5');
-		$this->assertAttribute($base . 'B@class', ' errorclassB');
+		$this->assertAttribute($base . 'B@class', 'errorclassB');
 
 		$this->type($base . 'B', 'Prado');
 		$this->click($base . 'ctl5');
-		$this->assertAttribute($base . 'B@class', ' errorclassB');
+		$this->assertAttribute($base . 'B@class', 'errorclassB');
 
 		$this->pause(50);
 		$this->type($base . 'B', 'test@prado.local');
