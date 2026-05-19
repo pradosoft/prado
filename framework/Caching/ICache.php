1c432c241fcdb08398b5d72e22dad18c4e9844f6
Belisoful <belisoful@icloud.com>
2026-05-19 23:43:56 +0000

    ICache - Adds future static function getIsAvailable, commented out. doc block update

--- framework/Caching/ICache.php
+++ framework/Caching/ICache.php
@@ -26,6 +26,7 @@ interface ICache
 	 * @return false|mixed the value stored in cache, false if the value is not in the cache or expired.
 	 */
 	public function get($id);
+
 	/**
 	 * Stores a value identified by a key into cache.
 	 * If the cache already contains such a key, the existing value and
@@ -34,29 +35,38 @@ interface ICache
 	 * @param string $id the key identifying the value to be cached
 	 * @param mixed $value the value to be cached
 	 * @param int $expire the number of seconds in which the cached value will expire. 0 means never expire.
-	 * @param ICacheDependency $dependency dependency of the cached item. If the dependency changes, the item is labelled invalid.
+	 * @param ICacheDependency $dependency dependency of the cached item. If the dependency changes, the item is labeled invalid.
 	 * @return bool true if the value is successfully stored into cache, false otherwise
 	 */
 	public function set($id, $value, $expire = 0, $dependency = null);
+
 	/**
 	 * Stores a value identified by a key into cache if the cache does not contain this key.
 	 * Nothing will be done if the cache already contains the key.
 	 * @param string $id the key identifying the value to be cached
 	 * @param mixed $value the value to be cached
 	 * @param int $expire the number of seconds in which the cached value will expire. 0 means never expire.
-	 * @param ICacheDependency $dependency dependency of the cached item. If the dependency changes, the item is labelled invalid.
+	 * @param ICacheDependency $dependency dependency of the cached item. If the dependency changes, the item is labeled invalid.
 	 * @return bool true if the value is successfully stored into cache, false otherwise
 	 */
 	public function add($id, $value, $expire = 0, $dependency = null);
+
 	/**
 	 * Deletes a value with the specified key from cache
 	 * @param string $id the key of the value to be deleted
 	 * @return bool if no error happens during deletion
 	 */
 	public function delete($id);
+
 	/**
 	 * Deletes all values from cache.
 	 * Be careful of performing this operation if the cache is shared by multiple applications.
 	 */
 	public function flush();
+
+	/**
+	 * Returns whether this cache backend's prerequisites are met.
+	 * @since 4.4.0
+	 */
+	//public static function getIsAvailable(): bool;
 }
