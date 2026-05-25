60024af47b59e834bde478465fc1924bb8ca7b2a
Belisoful <belisoful@icloud.com>
2026-05-25 00:47:31 +0000

    Fixed the broken SqlMap tests.db display in git - unit test fix

--- tests/unit/Data/SqlMap/common.php
+++ tests/unit/Data/SqlMap/common.php
@@ -73,10 +73,9 @@ class SQLiteBaseTestConfig extends BaseTestConfig
 		$this->_sqlmapConfigFile = SQLMAP_TESTS . '/sqlite.xml';
 		$this->_scriptDir = SQLMAP_TESTS . '/scripts/sqlite/';
 
-		$this->targetFile = realpath(SQLMAP_TESTS . '/sqlite/tests.db');
 		$this->baseFile = realpath(SQLMAP_TESTS . '/sqlite/backup.db');
-		$file = realpath($this->targetFile);
-		$this->_connection = new TDbConnection("sqlite:{$file}");
+		$this->targetFile = SQLMAP_TESTS . '/sqlite/tests.db';
+		$this->_connection = new TDbConnection("sqlite:{$this->targetFile}");
 	}
 
 	public function getScriptRunner()
